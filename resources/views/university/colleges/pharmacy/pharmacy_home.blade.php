1db7e6e892ea7e81c65571b1530ab337a4c95370
MathuR-A01 <mathuraayush037@gmail.com>
2024-08-29 17:51:44 +0530

    Added the FAqs data in the pharamcy college

--- resources/views/university/colleges/pharmacy/pharmacy_home.blade.php
+++ resources/views/university/colleges/pharmacy/pharmacy_home.blade.php
@@ -126,20 +126,17 @@
 
         </div>
 
-
-
-
-
-
-
         <div class="box-container">
             <div class="box">
-                <h1 class="college-main-heading">Teerthanker Mahaveer College of Pharmacy</h1>
+                <h1 class="college-main-heading">College of Pharmacy, Among the Top Colleges for Pharmacy Courses/Programmes</h1>
                 <div class="row">
                     <div class="col-12 px-2 px-md-5">
-                        <p style="text-align: justify;">The College of Pharmacyis committed to excellence in pharmaceutical education, research, and practice. As the best pharmacy college in UP, we strive to shape the next generation of pharmacy professionals and equip them with innovative techniques to resolve problems in drug industries and clinical areas.</p>
-
-                        <button class="tmu-btn btn-1 py-2 mt-4">Know More!</button>
+                        <p style="text-align: justify;">The College of Pharmacy was established in the year 2008 and approved by the Pharmacy Council of India. The college grooms professionals by providing diplomas, graduate, post-graduate and doctorate-level programmes, equipping the students with innovative techniques to resolve problems in drug industries and clinical areas.</p>
+                        <div class="read-more-content mt-0">
+                        <p style="text-align: justify;">Welcome to the College of Pharmacy at Teerthanker Mahaveer University (TMU), where academic excellence meets practical experience to shape the future leaders of the pharmaceutical world. Recognised as one of the top colleges for pharmacy courses in India, we offer a unique blend of theoretical knowledge and hands-on training, preparing our students for successful careers in the dynamic field of pharmacy.
+                        </p>
+                    </div>
+                    <a href="javascript:void(0);" class="tmu-btn btn-1 py-2 mt-4 read-more ms-2 mt-2" title="Read More">Read More</a>
 
                     </div>
 
@@ -162,7 +159,7 @@
                     <button class="nav-link" id="tab4-tab" data-bs-toggle="tab" data-bs-target="#Tab4" type="button" role="tab" aria-controls="Tab4" aria-selected="false"><i class="fas fa-building"></i> Infrastructure</button>
                 </li>
             </ul>
-            <div class="tab-content second ps-5 px-md-5" id="myTabContent">
+            <div class="tab-content second ps-4 px-md-5" id="myTabContent">
                 <div class="tab-pane fade show active" id="Tab1" role="tabpanel" aria-labelledby="tab1-tab">
                     <h3>Key Highlights</h3>
                     <ul class="m-ol-list ps-4">
@@ -717,8 +714,129 @@
                 </div>
             </div>
         </div>
-
 </section><!-- #content end -->
+
+
+<div class="container mb-5 mt-5">
+<h2 class="tmu-text-primary text-center"><span>Frequently Asked </span><span> Questions</span></h2>
+            <hr class="horizontal-line">
+
+            <button class="careers-accordion">Key Highlights of College of Pharmacy <span class="icon">+</span></button>
+            <div class="panel">
+            <ul class="m-ol-list ps-4 text-justify">
+    <li>🏆 UGC-Approved with NAAC A Accreditation: Our programs are recognized by the University Grants Commission (UGC) and accredited by the National Assessment and Accreditation Council (NAAC) with an A grade, ensuring high academic standards.</li>
+    <li>👩‍⚕️ Experiential Learning by Highly Qualified Faculties: Learn from experienced professionals who bring real-world insights into the classroom.</li>
+    <li>✨ 1st Private University in UP with a Drug Information Centre: Gain access to the only Drug Information Centre in Uttar Pradesh, providing essential drug-related information to healthcare professionals and consumers.</li>
+    <li>🏥 Hands-on Training in TMU Hospital: Benefit from practical training in our state-of-the-art hospital, bridging the gap between theory and practice.</li>
+    <li>🔬 Central Instrumentation Lab (CIL): Utilize advanced equipment and technologies for cutting-edge research and practical training.</li>
+    <li>💉 Well-equipped Laboratories and Medical Technology for Practical Training: Engage in hands-on learning with our sophisticated lab facilities.</li>
+    <li>✈️ Global Opportunities: Explore international exposure and collaborations with leading global pharmaceutical companies.</li>
+    <li>💼 100% Placement Assistance with an Excellent Track Record: Secure your future with our robust placement support and strong industry connections.</li>
+    <li>🌐 Strong Industry Connections for Real-World Insights: Benefit from our partnerships with top pharmaceutical companies and organizations.</li>
+    <li>🏫 Modern Classrooms for Interactive Sessions: Learn in an environment designed for interactive and effective teaching.</li>
+    <li>🎤 Pharmaceutical Workshops and Seminars: Participate in events that enhance your knowledge and skills.</li>
+    <li>🏋️‍♂️ Gym and Fitness Centre: Maintain a healthy lifestyle with our on-campus fitness facilities.</li>
+    <li>🏠 Comfortable Hostel Facilities: Enjoy a supportive and comfortable living environment.</li>
+    <li>🛍️ Shopping Stores and Cafeterias: Access essential amenities right on campus.</li>
+    <li>🚫 Anti-Ragging Campus: Study in a safe and respectful environment.</li>
+    <li>📶 Wi-Fi Enabled Campus: Stay connected with high-speed internet access.</li>
+</ul>
+
+            </div>
+            <!-- Add the horizontal line here -->
+            <hr class="horizontal-line">
+
+            <button class="careers-accordion">Explore Top-Notch Placement Opportunities<span class="icon">+</span></button>
+            <div class="panel">
+                <p class="text-justify">Our College of Pharmacy is not only a place of learning but also a gateway to numerous career opportunities. We offer comprehensive placement assistance to help our graduates secure positions in top pharmaceutical companies.</p>
+            </div>
+            <!-- Add the horizontal line here -->
+            <hr class="horizontal-line">
+            <button class="careers-accordion">Core Programs Offered with Diverse Specializations<span class="icon">+</span></button>
+            <div class="panel">
+            <ul class="m-ol-list ps-4 text-justify">
+                <li>BPharma</li>
+                <li>MPharma</li>
+                <li>DPharma</li>
+                <li>PharmD</li>
+                <li>Diploma</li>
+                <li>PhD</li>
+            </ul> 
+            <h3 class="tmu-text-primary fs-16"><span>The Best Private Pharmacy</span><span> College in UP</span></h3>   
+            <p class="text-justify">Established in 2008, the College of Pharmacy at TMU has consistently been at the forefront of pharmaceutical education, research, and practice. Our programs are designed to equip students with innovative techniques and skills to tackle challenges in drug industries and clinical areas</p>
+            </div>
+            <!-- Add the horizontal line here -->
+            <hr class="horizontal-line">
+            <button class="careers-accordion">Career Opportunities<span class="icon">+</span></button>
+            <div class="panel">
+                <p class="text-justify">A career in pharmacy offers numerous benefits, including:</p>
+                <ul class="m-ol-list ps-4 text-justify">
+                    <li><b class="fs-14">Diverse Job Roles:</b> Pharmacists can work in various settings, including hospitals, community pharmacies, research labs, and pharmaceutical companies.</li>
+                    <li><b class="fs-14">High Demand:</b> With an increasing focus on healthcare, the demand for qualified pharmacists is on the rise.</li>
+                    <li><b class="fs-14">Competitive Salaries:</b> Pharmacists often enjoy lucrative salaries and benefits.</li>
+                    <li><b class="fs-14">Global Opportunities:</b> Pharmaceutical professionals have the chance to work globally, contributing to healthcare systems worldwide.</li>
+                    <li><b class="fs-14">Innovative Field:</b> Pharmacy is a field that constantly evolves with new technologies and research.</li>
+                    <li><b class="fs-14">Patient Care:</b> Pharmacists play a crucial role in patient care, ensuring safe and effective medication use.</li>
+                    <li><b class="fs-14">Research Opportunities:</b> Engage in groundbreaking research that can lead to new drug discoveries.</li>
+                    <li><b class="fs-14">Entrepreneurial Ventures:</b> Pharmacists can start their own pharmacies or pharmaceutical businesses.</li>
+                    <li><b class="fs-14">Regulatory Roles:</b> Work with regulatory agencies to ensure the safety and efficacy of medications.</li>
+                    <li><b class="fs-14">Academic Careers:</b> Teach and mentor the next generation of pharmacy professionals.</li>
+                </ul>
+            </div>
+            <!-- Add the horizontal line here -->
+            <hr class="horizontal-line">
+            <button class="careers-accordion">Life at the College of Pharmacy <span class="icon">+</span></button>
+            <div class="panel">
+                <p class="text-justify">Life at TMU College of Pharmacy is vibrant and enriching. Our students enjoy:</p>
+                <h3 class="tmu-text-primary fs-16 mb-1"><span> State-of-the-</span><span>Art Facilities:</span></h3> 
+                <p class="text-justify">From modern classrooms to well-equipped labs, we provide an optimal learning environment.</p>
+                <h3 class="tmu-text-primary fs-16 mb-1"><span> Extracurricular </span><span> Activities:</span></h3> 
+                <p class="text-justify">Participate in various clubs, events, and seminars that enhance your educational experience.</p>
+                <h3 class="tmu-text-primary fs-16 mb-1"><span> Supportive </span><span> Community:</span></h3> 
+                <p class="text-justify">Be part of a diverse and inclusive community that supports your academic and personal growth.</p>
+            </div>
+            <!-- Add the horizontal line here -->
+            <hr class="horizontal-line">
+            <button class="careers-accordion">Conclusion <span class="icon">+</span></button>
+            <div class="panel">
+                <p class="text-justify">Join us at the College of Pharmacy, TMU, and embark on a transformative educational journey that prepares you for a successful career in pharmacy. For more details, visit <span class="highlight"><a href="https://www.tmu.ac.in/college-of-pharmacy" target="_blank"> TMU College of Pharmacy</a></span>.</p>
+            </div>
+            <hr class="horizontal-line">
+            <button class="careers-accordion">FAQs<span class="icon">+</span></button>
+            <div class="panel">
+            <p class="text-justify"><strong>Q: What is the duration of the B.Pharm course?</strong></p>
+            <p class="text-justify"><strong>Ans:</strong> The B.Pharm course duration is four years.</p>
+
+            <p class="text-justify"><strong>Q: What are the career opportunities after completing a B.Pharm?</strong></p>
+            <p class="text-justify"><strong>Ans:</strong> Graduates can pursue careers in community pharmacies, hospitals, pharmaceutical companies, research labs, and regulatory agencies.</p>
+
+            <p class="text-justify"><strong>Q: Does TMU provide placement assistance?</strong></p>
+            <p class="text-justify"><strong>Ans:</strong> Yes, TMU offers 100% placement assistance with an excellent track record.</p>
+
+            <p class="text-justify"><strong>Q: What makes TMU College of Pharmacy unique?</strong></p>
+            <p class="text-justify"><strong>Ans:</strong> Our NAAC A accreditation, state-of-the-art facilities, experiential learning approach, and strong industry connections set us apart.</p>
+
+            <p class="text-justify"><strong>Q: What specialisations are offered in the M.Pharm program?</strong></p>
+            <p class="text-justify"><strong>Ans:</strong> We offer specialisations in Pharmaceutics, Pharmacology, Pharmacognosy, and Pharmaceutical Analysis.</p>
+
+            <p class="text-justify"><strong>Q: Are there global opportunities for TMU pharmacy graduates?</strong></p>
+            <p class="text-justify"><strong>Ans:</strong> Yes, we have strong global connections and offer opportunities for international exposure and collaboration.</p>
+
+            <p class="text-justify"><strong>Q: What facilities are available on campus?</strong></p>
+            <p class="text-justify"><strong>Ans:</strong> Our campus features modern classrooms, well-equipped labs, a hospital for practical training, a gym, hostels, shopping stores, cafeterias, and a Wi-Fi-enabled environment.</p>
+
+            <p class="text-justify"><strong>Q: How does TMU support research in pharmacy?</strong></p>
+            <p class="text-justify"><strong>Ans:</strong> We provide advanced laboratories, sophisticated instruments, and a Drug Information Centre to support research and practical training.</p>
+
+            <p class="text-justify"><strong>Q: What is the role of the Drug Information Centre at TMU?</strong></p>
+            <p class="text-justify"><strong>Ans:</strong> The Drug Information Centre provides essential drug-related information to healthcare professionals and consumers, supporting medication safety and efficacy.</p>
+
+            <p class="text-justify"><strong>Q: How can I apply to TMU College of Pharmacy?</strong></p>
+            <p class="text-justify"><strong>Ans:</strong> You can apply through our website or contact our admissions office for more details on the application process.</p>
+
+            </div>
+            <hr class="horizontal-line">
+        </div>
 
 <br /><br />
 <script src="{{asset('/assets/js/tmimt.js')}}"></script>
