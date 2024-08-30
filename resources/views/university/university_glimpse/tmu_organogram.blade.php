822f2ee81639f52c07f21499a00b0c37404be5f9
MathuR-A01 <mathuraayush037@gmail.com>
2024-08-30 14:01:49 +0530

    replaced the path of the images in organogram and awrds page accord to img folder

--- resources/views/university/university_glimpse/tmu_organogram.blade.php
+++ resources/views/university/university_glimpse/tmu_organogram.blade.php
@@ -6,10 +6,10 @@
     <div class="container">
 
           <div class="row mx-0">
-            <h1 class="mb-2 tmu-text-primary tmu-page-heading" ><span>University </span><span class="d-block d-sm-inline"> Organogram</span></h1>
+            <h1 class="mb-2 tmu-text-primary tmu-page-heading mt-md-5" ><span>University </span><span class="d-block d-sm-inline"> Organogram</span></h1>
 
-            <div class="col-md-12 mb-4">
-              <img class="card-img-top" src="{{asset('/images/stock_images/TMU_organogram.jpg')}}" alt="Card image cap">
+            <div class="col-md-12 mb-4 mt-3">
+              <img class="card-img-top" src="{{asset('/assets/img/aboutus/organogram/TMU_organogram.jpg')}}" alt="Card image cap">
               <a href="#!">
                 <div class="mask rgba-white-slight"></div>
               </a>
