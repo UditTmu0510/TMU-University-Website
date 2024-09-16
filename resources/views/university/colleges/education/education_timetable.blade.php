247491c02ff774d204e355ad0fff11e2e3f18426
MathuR-A01 <mathuraayush037@gmail.com>
2024-09-16 13:34:31 +0530

    added the corrected path in the EDucation college directory

--- resources/views/university/colleges/education/education_timetable.blade.php
+++ resources/views/university/colleges/education/education_timetable.blade.php
@@ -5,13 +5,13 @@
             <div class="container d-none d-sm-block">
                 <div style="text-align:center">
                 <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Time </span><span > Table</span></h1>
-                <iframe src="https://www.tmu.ac.in/pdf/pdf/FacultyofEducationTT_2023_24.pdf" frameborder="0" height="800px" width="100%"></iframe>
+                <iframe src="{{asset('/assets/pdf/pdf/FacultyofEducationTT_2023_24.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
                 </div>
             </div>
             <div class="container d-block d-sm-none mb-3">
                 <div style="text-align:center">
                 <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Time </span><span > Table</span></h1>
-                <a href="https://www.tmu.ac.in/pdf/pdf/FacultyofEducationTT_2023_24.pdf" class="tmu-btn btn-4" download>Download PDF</a>
+                <a href="{{asset('/assets/pdf/pdf/FacultyofEducationTT_2023_24.pdf')}}" class="tmu-btn btn-4" download>Download PDF</a>
                 
                 </div>
             </div>
