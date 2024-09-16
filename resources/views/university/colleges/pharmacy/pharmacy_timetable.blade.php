625dc0434517357d0e4338410cd8f071775e6328
MathuR-A01 <mathuraayush037@gmail.com>
2024-09-16 13:39:20 +0530

    added the corrected path in the Pharmacy college directory

--- resources/views/university/colleges/pharmacy/pharmacy_timetable.blade.php
+++ resources/views/university/colleges/pharmacy/pharmacy_timetable.blade.php
@@ -6,13 +6,13 @@
     <div class="container d-none d-sm-block">
         <div style="text-align:center">
             <h1 class="tmu-text-primary tmu-page-heading"><span>Time </span><span> Table</span></h1>
-            <iframe src="https://www.tmu.ac.in/pdf/time_table/TT_TMCOP_29-09-2023.pdf" frameborder="0" height="800px" width="100%"></iframe>
+            <iframe src="{{asset('/assets/pdf/time_table/TT_TMCOP_29-09-2023.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
         </div>
     </div>
     <div class="container d-block d-sm-none mb-3">
         <div style="text-align:center">
             <h1 class="tmu-text-primary tmu-page-heading"><span>Time </span><span> Table</span></h1>
-            <a href="https://www.tmu.ac.in/pdf/time_table/TT_TMCOP_29-09-2023.pdf" class="tmu-btn btn-4" download>Download PDF</a>
+            <a href="{{asset('/assets/pdf/time_table/TT_TMCOP_29-09-2023.pdf')}}" class="tmu-btn btn-4" download>Download PDF</a>
 
         </div>
     </div>
