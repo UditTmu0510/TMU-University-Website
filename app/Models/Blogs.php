3cb854f4e616653816d86e429fb4b494281680df
Samkit-Webdev <samkit.webdev@tmu.ac.in>
2024-10-18 13:54:27 +0530

    Replaced the table name with new name

--- app/Models/Blogs.php
+++ app/Models/Blogs.php
@@ -9,6 +9,6 @@ class Blogs extends Model
 {
     use HasFactory;
     protected $guarded = [];
-    protected $table = 'post';
+    protected $table = 'blog_updated';
     protected $fillable = ['id','post_title','post_description','full_post','post_image','post_path','post_full_path','category','post_thumb','post_thumb_path','post_thumb_full_path','post_month','post_month_name','post_year','display_main','status','n_slug','popular_url','popular_priority','meta_title','meta_description','alt_tag','posted_at','priority','monaco_image_path','monaco_image_alt_tag','thumbnail_alt_tag','alt_tag_main_image'];
 }
