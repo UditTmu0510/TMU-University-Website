3ba0a94a750ed3abab76bd86792d2067b6ffb8ad
MathuR-A01 <mathuraayush037@gmail.com>
2024-10-18 13:06:34 +0530

    Added the recent added documents in the research directory

--- resources/views/university/research/phd_intake.blade.php
+++ resources/views/university/research/phd_intake.blade.php
@@ -4,214 +4,27 @@
 
 
 <div class="main-content">
-    <div class="container">
-      <!-- Write content for right side here  -->
-
-      <div class="row mx-0">
-        <h1 class="tmu-text-primary tmu-page-heading pt-md-5"><span>Intake for January </span><span> 2024 Cycle
-            of Ph.D. programme</span></h1>
-
-        <div class="row ps-4">
-          <div class="table-responsive">
-            <table class="table table-bordered table-striped">
-              <thead>
-                <tr>
-                  <th class="text-center">Sl. No.</th>
-                  <th>Name of Colleges</th>
-                  <th>Discipline</th>
-                  <th>Intake</th>
-                </tr>
-              </thead>
-              <tbody>
-
-                <tr>
-                  <td class="text-center" rowspan="7">1</td>
-                  <td rowspan="7">Faculty of Engineering</td>
-                  <td>Chemistry</td>
-                  <td class="text-center">30</td>
-                </tr>
-                <tr>
-                  <td>Physics</td>
-                  <td class="text-center">35</td>
-                </tr>
-                <tr>
-                  <td>Mathematics</td>
-                  <td class="text-center">22</td>
-                </tr>
-                <tr>
-                  <td>Electronics & Communication Engineering</td>
-                  <td class="text-center">13</td>
-                </tr>
-                <tr>
-                  <td>Electrical Engineering</td>
-                  <td class="text-center">15</td>
-                </tr>
-                <tr>
-                  <td>Mechanical Engineering</td>
-                  <td class="text-center">11</td>
-                </tr>
-                <tr>
-                  <td>Civil Engineering</td>
-                  <td class="text-center">1</td>
-                </tr>
-
-                <tr>
-                  <td class="text-center" rowspan="7">2</td>
-                  <td rowspan="7">Dental College & Research Centre</td>
-                  <td>Oral Medical & Radiology</td>
-                  <td class="text-center">21</td>
-                </tr>
-                <tr>
-                  <td>Public Health Dentistry</td>
-                  <td class="text-center">21</td>
-                </tr>
-                <tr>
-                  <td>Pedodontics & Preventive Dentistry</td>
-                  <td class="text-center">14</td>
-                </tr>
-                <tr>
-                  <td>Orthodontics and Dentofacial Orthopaedics</td>
-                  <td class="text-center">9</td>
-                </tr>
-                <tr>
-                  <td>Oral &amp; Maxillofacial Pathology</td>
-                  <td class="text-center">7</td>
-                </tr>
-                <tr>
-                  <td>Conservative Dentistry and Endodontics</td>
-                  <td class="text-center">7</td>
-                </tr>
-                <tr>
-                  <td>Periodontology</td>
-                  <td class="text-center">8</td>
-                </tr>
-
-
-                <tr>
-                  <td class="text-center">3</td>
-                  <td>College of Education</td>
-                  <td> Education</td>
-                  <td class="text-center">2</td>
-                </tr>
-                <tr>
-                  <td class="text-center" rowspan="4">4</td>
-                  <td rowspan="4">College of Pharmacy</td>
-                  <td>Pharmacy (Pharmacognosy)</td>
-                  <td class="text-center">4</td>
-                </tr>
-                <tr>
-                  <td>Pharmacy (Pharmaceutical Chemistry)</td>
-                  <td class="text-center">8</td>
-                </tr>
-                <tr>
-                  <td>Pharmacy (Pharmacology)</td>
-                  <td class="text-center">2</td>
-                </tr>
-                <tr>
-                  <td>Pharmacy (Pharmaceutics)</td>
-                  <td class="text-center">2</td>
-                </tr>
-
-
-
-                <!--<tr>-->
-                <!--	<td class="text-center">6</td>-->
-                <!--	<td>College of Nursing</td>-->
-                <!--	<td>Nursing</td>-->
-                <!--	<td class="text-center">9</td>-->
-                <!--</tr>-->
-
-                <tr>
-                  <td class="text-center">5</td>
-                  <td>Teerthanker Mahaveer Institute of Management & Technology</td>
-                  <td>Management</td>
-                  <td class="text-center">12</td>
-                </tr>
-                <tr>
-                  <td class="text-center" rowspan="5">6</td>
-                  <td rowspan="5">Medical College & Research Centre</td>
-                  <td>Medical – Anatomy</td>
-                  <td class="text-center">8</td>
-                </tr>
-                <tr>
-                  <td>Medical – Pharmacology</td>
-                  <td class="text-center">7</td>
-                </tr>
-                <tr>
-                  <td>Medical – Microbiology</td>
-                  <td class="text-center">5</td>
-                </tr>
-                <tr>
-                  <td>Medical – Biochemistry</td>
-                  <td class="text-center">13</td>
-                </tr>
-                <tr>
-                  <td>Medical – Physiology</td>
-                  <td class="text-center">16</td>
-                </tr>
-                <tr>
-                  <td class="text-center" rowspan="3">7</td>
-                  <td rowspan="3">College of Paramedical Sciences</td>
-                  <td>Biochemistry</td>
-                  <td class="text-center">7</td>
-                </tr>
-                <tr>
-                  <td>Optometry</td>
-                  <td class="text-center">4</td>
-                </tr>
-                <!--
-									<tr>
-										<td>Radio Imaging Techniques</td>
-										<td class="text-center">5</td>
-									</tr>
-									-->
-                <tr>
-                  <td>Radiodiagnosis / Radiology</td>
-                  <td class="text-center">1</td>
-                </tr>
-
-                <tr>
-                  <td class="text-center" rowspan="2">8</td>
-                  <td rowspan="2">College of Computing Sciences & IT</td>
-                  <td>Computer Applications </td>
-                  <td class="text-center">13</td>
-                </tr>
-                <tr>
-                  <td>Computer Science Engineering</td>
-                  <td class="text-center">21</td>
-                </tr>
-                <tr>
-                  <td class="text-center">9</td>
-                  <td>College of Law &amp; Legal Studies</td>
-                  <td>Law</td>
-                  <td class="text-center">9</td>
-                </tr>
-                <tr>
-                  <td class="text-center">10</td>
-                  <td>Department of Physiotherapy</td>
-                  <td>Physiotherapy</td>
-                  <td class="text-center">4</td>
-                </tr>
-                <tr>
-                  <td class="text-center">11</td>
-                  <td>Centre for Jain Studies</td>
-                  <td>Jainology</td>
-                  <td class="text-center">6</td>
-                </tr>
-                <tr>
-                  <td class="text-center">12</td>
-                  <td>College of Physical Education</td>
-                  <td>Physical Education</td>
-                  <td class="text-center">3</td>
-                </tr>
-
-              </tbody>
-            </table>
-          </div>
+  <div class="container">
+    <div class="row mx-0 mb-3">
+      <div class="container d-none d-sm-block">
+        <div style="text-align:center">
+          <h1 class="tmu-text-primary tmu-page-heading pt-md-5"><span>Intake for January 2025  </span> <span> Cycle of Ph.D. programme</span> </h1>
+          <iframe src="{{asset('/assets/pdf/research/intake/PhD_Intake_for_TRAT_2025.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
+        </div>
+      </div>
+      <div class="container d-block d-sm-none mb-3">
+        <div style="text-align:center">
+          <h1 class="tmu-text-primary tmu-page-heading pt-md-5"><span>Intake for January 2025  </span> <span> Cycle of Ph.D. programme</span> </h1>
+          <a href="{{asset('/assets/pdf/research/intake/PhD_Intake_for_TRAT_2025.pdf')}}" class="tmu-btn btn-4" download>Download PDF</a>
         </div>
       </div>
     </div>
   </div>
 </div>
+</div>
+
+
+
+
 
 @endsection
