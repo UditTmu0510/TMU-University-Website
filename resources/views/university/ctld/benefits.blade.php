32652f5ade5b036f364c921705714ee7c16f65bf
MathuR-A01 <mathuraayush037@gmail.com>
2024-10-18 11:38:55 +0530

    added the ctld benfits page

---
+++ resources/views/university/ctld/benefits.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.university.departments.ctld_with_sidebar')
+@section('content')
+
+
+<link rel="stylesheet" href="{{asset('/assets/css/tmu-timeline.css')}}">
+    <div class="main-content">
+        <div class="container">
+            <!-- Experience Start -->
+            <div class="experience" id="experience">
+                <div class="container">
+                    <header class="section-header text-center wow zoomIn">
+                        <h2 class="tmu-text-primary"><span>Benefits of  </span><span> CTLD Training</span></h2>
+                    </header>
+                    <div class="timeline ">
+                        <div class="timeline-item left wow slideInLeft">
+                            <div class="timeline-text">
+                                <div class="timeline-date">Boosting Confidence</div>
+                                <p class="text-justify">This is most important trait of any successful person. Trainers of CTLD, boost the confidence of students by making them participate in activities like public speaking, group discussion, mock interviews, Team work, creativity and innovation, quantitative aptitude problem solving etc.</p>
+                            </div>
+                        </div>
+                        <div class="timeline-item right wow slideInRight">
+                            <div class="timeline-text">
+                                <div class="timeline-date">Upgrading Skills in Staff & Students</div>
+                                <p class="text-justify">Trainers of CTLD plan and execute various programmes for faculty and staff development (FDPs & SDPs) on continuous basis to develop teaching & behavioural skills.</p>
+                            </div>
+                        </div>
+                        <div class="timeline-item left wow slideInLeft">
+                            <div class="timeline-text">
+                                <div class="timeline-date">Making Students Industry Ready</div>
+                                <p class="text-justify">
+                                There is a severe competition in all the industries and only highly developed employees can keep the company ahead of others. Hence, companies select those students who are excellent in domain skills as well as soft skills and aptitude. Most higher education institutes focus only on domain skills. However, at TMU we ensure all round development of our students. CTLD plays a major role in developing soft skills and quantitative aptitude during the entire program through rigorous training.</p>
+                            </div>
+                        </div>
+                        <div class="timeline-item right wow slideInRight">
+                            <div class="timeline-text">
+                                <div class="timeline-date">Better Placement </div>
+                                <p class="text-justify">CTLD has a strong team of 17 trainers, who have long experience of preparing students for placements into world-class companies. They are always aware of current skills requirements of the companies; hence, they prepare students in planned and systematic way. After creation of CTLD, the placements of TMU have improved phenomenally.</p>
+                            </div>
+                        </div>
+                        <div class="timeline-item left wow slideInLeft mb-0">
+                            <div class="timeline-text">
+                                <div class="timeline-date">Better Reviews for the Brand</div>
+                                <p class="text-justify">Brand reviews improve, when the customers are satisfied with the brand. At brand TMU, we ensure that our customers (students & their parents) are not only satisfied but are delighted with our services. CTLD has major role in brand building by training faculty & staff members and each students, thereby ensuring better brand reviews.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                   
+                        <div class="timeline mt-0">
+                            <div class="timeline-item right wow slideInRight">
+                                <div class="timeline-text">
+                                    <div class="timeline-date">Qualitative Development in <br> the Students & Staff</div>
+                                    <p class="text-justify">The trainers of CTLD always ensure that the personality of each student is groomed to the level that makes them stand apart from the students of other Institutions. Our students have better manners and etiquette. We ensure that our students achieve aristocracy. CTLD also takes care of soft skills development of every staff members.</p>
+                                </div>
+                            </div>
+                            
+                        </div>
+                </div>
+            </div>
+            <!-- Job Experience End -->
+        </div>
+    </div>
+</div>
+</div>
+@endsection
