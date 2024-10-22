b0ed04f962037c6cf1238a4653f1690a772293a4
MathuR-A01 <mathuraayush037@gmail.com>
2024-10-22 12:54:09 +0530

    added the updated image & designation of the nursing principal

--- resources/views/university/colleges/nursing/nursing_viceprincipal.blade.php
+++ resources/views/university/colleges/nursing/nursing_viceprincipal.blade.php
@@ -1,31 +1,37 @@
 @extends('layouts.university.colleges.nursing_with_sidebar')
 @section('content')
 
+<style>
+    @media (max-width: 767.98px) {
+        .quote-box {
+            width: 100%;
+            background-color: transparent;
+            box-shadow: none;
+            padding: 0;
+        }
+    }
+</style>
 <div class="main-content mt-3 mt-sm-0 pt-2 pt-sm-5">
     <div class="container">
-
-
-
         <div class="">
-            <h1 class="tmu-text-primary tmu-page-heading"><span>Message from the </span><span>Vice Principal's Desk</span></h1>
+            <h1 class="tmu-text-primary tmu-page-heading"><span>Message from the </span><span> Principal's Desk</span></h1>
             <div class="quote-box mx-auto">
                 <div class="profile-image mb-2">
-                    <img src="{{asset('/assets/img/principal/nursing/nursing_vice_principal.jpg')}}" alt="Profile Image">
+                    <img src="{{asset('/assets/img/principal/nursing/tmns_head.jpg')}}" alt="Profile Image">
                 </div>
                 <div class="quote-text mt-4 mt-md-0">
-                    <p class="text-justify text-dark fs-14 mb-2"><i class="bi bi-quote" style="font-size:30px; color: #737373;"></i>Caring is the essence of nursing, and learning is the path to care better...... </p>
-                    <p class="text-justify text-dark fs-14 mb-2">As Vice Principal of the esteemed Teerthanker Mahaveer College of Nursing, I would like to take a moment to address all of you and express my heartfelt gratitude for your unwavering dedication and commitment to excellence.</p>
-                <p class="text-justify text-dark fs-14 mb-2">We offer programmes ranging from Auxiliary Nurse Midwife (ANM), General Nursing and Midwifery (GNM), BSc Nursing, and postgraduate programmes like MSc Nursing and PhD in Nursing. Our college is recognised by the UP State Nursing Council and the Indian Nursing Council (INC).</p>
-                <p class="text-justify text-dark fs-14 mb-2">I want to express my deepest appreciation to our exceptional faculty members, who demonstrate extraordinary professionalism and adaptability in transitioning and grooming nursing students into competent healthcare professionals. To all our exceptional students, your unwavering passion for nursing and commitment to caring for others have made us proud. Their adaptability and perseverance in navigating the ever-changing healthcare landscape have been truly remarkable.</p>
-                <p class="text-justify text-dark fs-14 mb-2">I have immense faith in the college’s practices that instil in students the true essence of being healthcare professionals. We have nurtured and shaped professionals who have made significant contributions to the nursing profession and positively impacted the lives of countless individuals.</p>
-                <p class="text-justify text-dark fs-14 mb-2">As we move forward, let us continue to embrace change, pursue knowledge, and strive for excellence. Together, we can overcome any obstacles that come our way and continue providing compassionate and competent care to those in need.</p>
+                    <p class="text-justify text-dark fs-14 mb-2"><i class="bi bi-quote" style="font-size:30px; color: #737373;"></i>Caring is the essence of nursing, and learning is the path to care better..... </p>
+                    <p class="text-justify text-dark fs-14 mb-2">As Principal of the esteemed Teerthanker Mahaveer College of Nursing, I would like to take a moment to address all of you and express my heartfelt gratitude for your unwavering dedication and commitment to excellence.</p>
+                    <p class="text-justify text-dark fs-14 mb-2">We offer programmes ranging from Auxiliary Nurse Midwife (ANM), General Nursing and Midwifery (GNM), BSc Nursing, and postgraduate programmes like MSc Nursing and PhD in Nursing. Our college is recognised by the UP State Nursing Council and the Indian Nursing Council (INC).</p>
+                    <p class="text-justify text-dark fs-14 mb-2">I want to express my deepest appreciation to our exceptional faculty members, who demonstrate extraordinary professionalism and adaptability in transitioning and grooming nursing students into competent healthcare professionals. To all our exceptional students, your unwavering passion for nursing and commitment to caring for others have made us proud. Their adaptability and perseverance in navigating the ever-changing healthcare landscape have been truly remarkable.</p>
+                    <p class="text-justify text-dark fs-14 mb-2">I have immense faith in the college’s practices that instil in students the true essence of being healthcare professionals. We have nurtured and shaped professionals who have made significant contributions to the nursing profession and positively impacted the lives of countless individuals.</p>
+                    <p class="text-justify text-dark fs-14 mb-2">As we move forward, let us continue to embrace change, pursue knowledge, and strive for excellence. Together, we can overcome any obstacles that come our way and continue providing compassionate and competent care to those in need.</p>
+                    <p class="text-justify text-dark fs-14 mb-2">I extend heartiest wishes to teachers of today for preparing teachers of tomorrow.....</p>
                     <p class="mt-3">- Dr (Prof) Jaslin M </p>
-                    <p>Vice Principal</p>
+                    <p>Acting Principal</p>
                 </div>
             </div>
         </div>
-
-
     </div>
 </div>
 </div>
