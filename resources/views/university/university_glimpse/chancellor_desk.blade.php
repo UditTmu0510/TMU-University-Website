adb2da2d20b9be6f307c4028b1d481091b6eb8ac
Samkit-Webdev <samkit.webdev@tmu.ac.in>
2024-10-23 13:19:50 +0530

    Changed the national highway 9, 144 km from delhi and added margin between paragrapn in chancellor, VC, FVC pages

--- resources/views/university/university_glimpse/chancellor_desk.blade.php
+++ resources/views/university/university_glimpse/chancellor_desk.blade.php
@@ -14,7 +14,7 @@
                                 <img src="{{asset('assets/img/aboutus/administration/chancellor-image.png')}}" class="w-100" alt="">
                             </div>
                             <div class="col-md-8">
-                                <p class="person-quote px-4 px-md-5">
+                                <p class="person-quote px-4 px-md-5 pb-2">
                                     <!-- <i class="bi bi-quote"></i> -->
                                     <!-- &emsp;&emsp; -->
                                     It is my firm belief that harmonious development of the society can be achieved by
@@ -24,7 +24,8 @@
                                     powerful instrument of socio-economic change can transform the attitude, thinking
                                     and life style of the people and society. <span class="highlight">Education enables
                                         flexible thinking and better decision making capabilities in
-                                        individuals</span>.<br>
+                                        individuals</span>.<br></p>
+                                        <p class="person-quote px-4 px-md-5 pb-2">
                                     I have always dreamt of empowering the society with enlightened, skillful and
                                     socially relevant citizens, which can be possible only by educating the masses. As
                                     such, we initiated in this benign task of reconstructing the social order by
@@ -32,19 +33,23 @@
                                     in our own humble way. <span class="highlight">Teerthanker Mahaveer University has
                                         taken the task of developing human capital in tune with the requirements of
                                         industry and the society</span> by integrating the basic principles of Right
-                                    Philosophy, Right Knowledge and Right Conduct in its policies and programmes.<br>
+                                    Philosophy, Right Knowledge and Right Conduct in its policies and programmes.<br></p>
+                                    <p class="person-quote px-4 px-md-5 pb-2">
                                     The University stands for humanism, tolerance, reason, adventure of ideas and the
                                     search of truth. We ensure that multitude of students study in a tolerant but
                                     encouraging environment that provides adequate liberty for individual ambition and
-                                    lifestyle.<br>
+                                    lifestyle.<br></p>
+                                    <p class="person-quote px-4 px-md-5 pb-2">
                                     The multidisciplinary approach of our curriculum focuses on <span
                                         class="highlight">experiential learning</span>, leveraging of <span
                                         class="highlight">ICT for better content delivery</span>, and provisions for
                                     <span class="highlight">sports and extracurricular activities</span>, which are
-                                    integral components of the learning process.<br>
+                                    integral components of the learning process.<br></p>
+                                    <p class="person-quote px-4 px-md-5 pb-2">
                                     I extend my best wishes to all the aspirants and assure that under the mentorship of
                                     dedicated faculty of this prestigious University, they shall be able to <span
                                         class="highlight">take full advantage of opportunities offered to them</span>.
+                                    </p>
 
                                     <!-- <br> -->
                                     <!-- <img src="https://signature.freefire-name.com/img.php?f=7&t=Suresh%20Jain" alt="" class="w-25"> -->
