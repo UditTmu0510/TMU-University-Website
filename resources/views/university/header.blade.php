33d58fe15ab252433315181c4a1cd92016792dce
Ug0510 <uditgg0510@gmail.com>
2024-10-24 16:42:33 +0530

    maintain the js and some css

--- resources/views/university/header.blade.php
+++ resources/views/university/header.blade.php
@@ -15,12 +15,9 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800&display=swap"
         rel="stylesheet"> -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="{{asset('assets/css/default/bootstrap.min.css')}}"> -->
+   
 
-
-    <script>
+        <script>
   // Function to load a stylesheet
   function loadStyleSheet(href) {
     var link = document.createElement('link');
@@ -49,12 +46,25 @@
     this.rel = 'stylesheet';
   };
   document.head.appendChild(coreStyleLink);
+
+  // Load Bootstrap CSS
+  var bootstrapLink = document.createElement('link');
+  bootstrapLink.rel = 'preload';
+  bootstrapLink.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css";
+  bootstrapLink.as = 'style';
+  bootstrapLink.onload = function() {
+    this.rel = 'stylesheet';
+  };
+  document.head.appendChild(bootstrapLink);
 </script>
 
 <noscript>
   <link rel="stylesheet" href="{{ asset('assets/css/default/fontawesome6.2.1.all.min.css') }}">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </noscript>
+
+   
 
     <!-- Font Icons -->
     <link rel="stylesheet" href="{{asset('css/font-icons.css')}}">
