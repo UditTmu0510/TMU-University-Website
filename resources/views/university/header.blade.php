32e9ed7b1b8a542daad33ba52cee09a17b16006f
Samkit-Webdev <samkit.webdev@tmu.ac.in>
2024-10-25 12:11:15 +0530

    changing the calling of the owl-carousel

--- resources/views/university/header.blade.php
+++ resources/views/university/header.blade.php
@@ -71,6 +71,7 @@
         <link rel="stylesheet" href="{{ asset('assets/css/default/fontawesome6.2.1.all.min.css') }}">
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     </noscript>
 
 
@@ -87,7 +88,7 @@
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{asset('css/custom.css')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <link defer rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"> -->
+    
     <!-- Niche Demos -->
     <link rel="stylesheet" href="{{asset('demos/insurance/insurance.css')}}">
 
