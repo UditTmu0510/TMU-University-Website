b8bee622bb7ecb60d043348f022dbf6bb750f87a
Ug0510 <uditgg0510@gmail.com>
2024-10-28 12:31:35 +0530

    fix a issue in college_menubar.js

--- public/assets/js/college_menubar.js
+++ public/assets/js/college_menubar.js
@@ -1174,20 +1174,20 @@ window.addEventListener('DOMContentLoaded', function () {
     setTimeout(function () {
 
         // Fade Preloader
-        document.querySelector(".preloader").style.display = "none";
+        // document.querySelector(".preloader").style.display = "none";
 
         // Show menu button
-        document.getElementById('menubar--open--button').style.visibility = 'visible';
+        // document.getElementById('menubar--open--button').style.visibility = 'visible';
 
         // Making body accessible
         document.querySelector('body').style.height = 'auto';
         document.querySelector('body').style.overflowY = 'scroll';
 
-        const wrapper = document.getElementById('wrapper');
+        // const wrapper = document.getElementById('wrapper');
 
-        if (wrapper) {
-            wrapper.style.visibility = 'visible';
-        }
+        // if (wrapper) {
+        //     wrapper.style.visibility = 'visible';
+        // }
 
 
     });
