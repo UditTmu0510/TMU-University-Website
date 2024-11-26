1d234ca57670a295bbd4b3198f6b543dcb009018
MathuR-A01 <mathuraayush037@gmail.com>
2024-11-26 13:17:36 +0530

    added teh recently received data into the directory

--- resources/views/university/colleges/medical/medical_aebas_attendance.blade.php
+++ resources/views/university/colleges/medical/medical_aebas_attendance.blade.php
@@ -96,11 +96,11 @@
                                                 <td class="text-center">25.11.2024</td>
                                                 <td class="text-center"><a href="{{asset('/assets/pdf/aebas-attendance/november/25th-Novemebr-2024.pdf')}}" target="_blank"><i class="fa fa-file" aria-hidden="true"></i></a></td>
                                             </tr>
-                                            <!-- <tr>
+                                            <tr>
                                                 <td class="text-center">12.</td>
                                                 <td class="text-center">26.11.2024</td>
-                                                <td class="text-center"><a href="{{asset('/assets/pdf/aebas-attendance/november/23rd-November-2024.pdf')}}" target="_blank"><i class="fa fa-file" aria-hidden="true"></i></a></td>
-                                            </tr> -->
+                                                <td class="text-center"><a href="{{asset('/assets/pdf/aebas-attendance/november/26th-Novemebr-2024.pdf')}}" target="_blank"><i class="fa fa-file" aria-hidden="true"></i></a></td>
+                                            </tr>
                                         </tbody>
                                     </table>
                                 </div>
