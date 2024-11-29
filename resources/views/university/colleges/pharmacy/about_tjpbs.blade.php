7d7de7c280b3da14043c03b200bf0f1f930afa66
MathuR-A01 <mathuraayush037@gmail.com>
2024-11-29 12:23:55 +0530

    data will be updated soon

--- resources/views/university/colleges/pharmacy/about_tjpbs.blade.php
+++ resources/views/university/colleges/pharmacy/about_tjpbs.blade.php
@@ -1,10 +1,27 @@
 @extends('layouts.university.colleges.pharmacy_tjpbs_with_sidebar')
 @section('content')
 
+<style>
+
+</style>
 
 
+<div class="main-content">
+    <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>TMU Journal of Pharmaceutical and  </span><span> Biological Sciences (TJPBS)</span></h1>
+    <div class="container">
+            <div class="row mb-3">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th class="text-center fw-bold"> Data Will be updated Soon! </th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
 
-
-
+</div>
 </div>
 @endsection
