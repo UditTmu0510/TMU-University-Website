5a819d2e2926dda596645afcfd3f13b1565d07e7
Samkit Modi <sammodtab@gmail.com>
2024-12-02 16:44:48 +0530

    added hr to author guidelines

--- resources/views/university/colleges/pharmacy/tjpbs_authors_guidelines.blade.php
+++ resources/views/university/colleges/pharmacy/tjpbs_authors_guidelines.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.university.colleges.pharmacy_tjpbs_with_sidebar')
 @section('content')
 
+<style>
+    hr {
+        width: 40%;
+        /* Sets the width to 30% of the parent container */
+        margin: 20px auto;
+        /* Centers the <hr> horizontally */
+        border: 1px solid #000000b5;
+        /* Optional: defines the thickness and color */
+    }
+</style>
+
 <div class="main-content">
     <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Authors </span><span>Guidelines</span></h1>
     <section class="about-section bg-light mt-4 p-3">
@@ -15,12 +26,14 @@
                     margins of at least one inch on all sides. Pages should be numbered consecutively.</li>
             </ul>
 
-            <h3 class="mb-2 pb-0 pt-4 tmu-text-primary fw-bold h3size"><span>Communication to</span><span> the Journal</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 pt-3 tmu-text-primary fw-bold h3size"><span>Communication to</span><span> the Journal</span></h3>
             <p class="about-p pb-4 text-justify">
                 All the articles must be communicated through journal email-……………….
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Structure of</span><span> the Manuscript</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Structure of</span><span> the Manuscript</span></h3>
             <ul class="m-ol-list ps-5 mb-4">
                 <li>TITLE</li>
                 <li>ABSTRACT (not exceeding 250 words)</li>
@@ -36,12 +49,13 @@
                 <li>REFERENCES</li>
             </ul>
             <p class="about-p pb-4 text-justify">
-                <b>Note:</b>Divide your article into clearly defined sections. Heading should be typed in
+                <b>Note:</b> Divide your article into clearly defined sections. Heading should be typed in
                 upper case, bold. Sub-headings should be typed in sentence case, italics. Each heading
                 should appear on its own separate line.
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Title</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Title</span></h3>
             <p class="about-p pb-4 text-justify">
                 Title page contains title of the manuscript in bold face, Title case (font size 14, Arial Black,
                 in Bold), names of the authors in title case (font size 12, Times New Roman, Bold) followed
@@ -58,25 +72,29 @@
                 address.
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Abstract</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Abstract</span></h3>
             <p class="about-p pb-4 text-justify">
                 A structured abstract up to 250 words, should be written including objective, methods, results
                 and conclusion.
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Keywords</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Keywords</span></h3>
             <p class="about-p pb-4 text-justify">
                 Up to 4-6 keywords must be provided in alphabetical order.
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Introduction</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Introduction</span></h3>
             <p class="about-p pb-4 text-justify">
                 The introduction should start on a new page after the abstract and key-words. The
                 introduction should provide a clear statement of the problem, the relevant recent literature on
                 the subject, objectives of the study, and the proposed approach or relevant findings.
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Materials &</span> <span> Methods</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Materials &</span> <span> Methods</span></h3>
             <p class="about-p pb-4 text-justify">
                 Documentation of methodology used should be sufficient to permit replication of the
                 research. State the source of specialized materials, diets, chemicals, and instruments and other
@@ -92,43 +110,50 @@
             </p>
 
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Results</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Results</span></h3>
             <p class="about-p pb-4 text-justify">
                 The important results of the work should be clearly stated and illustrated where necessary by
                 tables and figures. The statistical treatment of data and the significance level of the factors
                 should be stated wherever necessary.
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Discussion</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Discussion</span></h3>
             <p class="about-p pb-4 text-justify">
                 This should explore the significance of the results of the work, not repeat them. A combined
                 result and discussion section is often appropriate. Avoid extensive citations and discussion of
                 published literature.
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Conclusions</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Conclusions</span></h3>
             <p class="about-p pb-4 text-justify">
                 A brief paragraph summarizing the significant findings of the work is required.
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Conflict of</span> <span> Interest</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Conflict of</span> <span> Interest</span></h3>
             <p class="about-p pb-4 text-justify">
                 Authors should clearly state about any conflict in the manuscript.
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Acknowledgments</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Acknowledgments</span></h3>
             <p class="about-p pb-4 text-justify">
                 List here those individuals who provided help during the research(ex- providing language
                 help, writing assistance or proof reading the article, etc).
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Funding</span> <span>Source </span> </h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Funding</span> <span>Source </span> </h3>
             <p class="about-p pb-4 text-justify">
                 Please list the source(s) of funding for the study, for each author, and for the manuscript
                 preparation.
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>References</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>References</span></h3>
             <p class="about-p pb-4 text-justify">
                 Citations of literature within the text must be presented in numerical order and should be set
                 in square brackets, thus [1, 2]. The cited literatures are also collected in numerical order at the
@@ -162,7 +187,8 @@
             </ul>
 
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Figures</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Figures</span></h3>
             <ul class="m-ol-list ps-5 mb-4">
                 <li>Figures should be numbered consecutively with Arabic numerals (i.e. Figure 1, 2, 3, etc.).</li>
                 <li>Provide a brief legend for each figure.</li>
@@ -182,13 +208,15 @@
             </ul>
 
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Tables</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Tables</span></h3>
             <p class="about-p pb-4 text-justify">
                 Tables should be editable text and numbered consecutively. Each table must have a title, with notes below the body. Avoid vertical lines; use minimal horizontal lines.
             </p>
 
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Review</span><span> Article</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Review</span><span> Article</span></h3>
             <p class="about-p pb-4 text-justify">
                 Review articles should contain up-to-date information providing comprehensive coverage of
                 relevant literature. Review articles should preferably be written by scientists who have in-
@@ -199,7 +227,8 @@
             </p>
 
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Short</span><span> Communications</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Short</span><span> Communications</span></h3>
             <p class="about-p pb-4 text-justify">
                 The research and technical communications section of this journal (maximum 2,000 words) is
                 open to interesting results worthy of publication without requiring extensive introduction and
@@ -209,7 +238,8 @@
                 papers.
             </p>
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Case</span><span> Reports</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Case</span><span> Reports</span></h3>
             <p class="about-p pb-4 text-justify">
                 Interesting clinical cases may be considered for publication. Those with photographs stand a
                 better chance. The case reports should have an unstructured abstract, introduction, case
@@ -217,7 +247,8 @@
             </p>
 
 
-            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size"><span>Copyright</span><span> Transfer Agreement</span></h3>
+            <hr>
+            <h3 class="mb-2 pb-0 tmu-text-primary fw-bold h3size pt-3"><span>Copyright</span><span> Transfer Agreement</span></h3>
             <p class="about-p pb-4 text-justify">
                 Authors will be asked to sign a transfer of copyright form when the papers are accepted for
                 publication and be typeset. All authors must read and agree to the conditions of copyright
