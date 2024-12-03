740ac34354a3420f4f42f8ff92f5c9894ccf7a61
Ug0510 <uditgg0510@gmail.com>
2024-12-03 15:06:34 +0530

    converge the 2 H2 into agriculture academic calendar blade

--- resources/views/university/colleges/agriculture/agriculture_academic_calendar.blade.php
+++ resources/views/university/colleges/agriculture/agriculture_academic_calendar.blade.php
@@ -3,16 +3,11 @@
 
 
 <div class="main-content">
-    <div class="container d-none d-sm-block">
+    <div class="container ">
         <div style="text-align:center">
             <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Academic</span><span> Calendar</span></h1>
-            <iframe src="{{asset('assets/pdf/academic_calendars/TMDC&CRC-Academic-Calendar-23-24.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
-        </div>
-    </div>
-    <div class="container d-block d-sm-none mb-3">
-        <div style="text-align:center">
-            <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Academic</span><span> Calendar</span></h1>
-            <a href="{{asset('assets/pdf/academic_calendars/TMDC&CRC-Academic-Calendar-23-24.pdf')}}" class="tmu-btn btn-4" download>Download PDF</a>
+            <iframe src="{{asset('assets/pdf/academic_calendars/TMDC&CRC-Academic-Calendar-23-24.pdf')}}" frameborder="0" height="800px" width="100%" class="d-none d-sm-block"></iframe>
+            <a href="{{asset('assets/pdf/academic_calendars/TMDC&CRC-Academic-Calendar-23-24.pdf')}}" class="tmu-btn btn-4 d-block d-sm-none" download>Download PDF</a>
         </div>
     </div>
 </div>
