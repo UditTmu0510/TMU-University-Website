81b70a371c5e5beb70ffe88f49ca3626e16a6f38
MathuR-A01 <mathuraayush037@gmail.com>
2024-12-09 13:52:57 +0530

    added the TT into teh directory

--- resources/views/university/colleges/agriculture/agriculture_timetable.blade.php
+++ resources/views/university/colleges/agriculture/agriculture_timetable.blade.php
@@ -11,8 +11,29 @@
     <div class="container d-block d-sm-none mb-3">
         <div style="text-align:center">
             <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Time </span><span> Table</span></h1>
-            <a href="{{asset('/assets/pdf/time_table/Agri_TT_24_25.pdf')}}" class="tmu-btn btn-4" download>Download PDF</a>
+        </div>
 
+        <div class="iqac-container">
+            <div class="iqac-card">
+                <div class="card-pdf">
+                    <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
+                    <p class="description">Time Table (Academic Session 2024-25)</p>
+                    <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12"
+                        href="{{asset('/assets/pdf/time_table/Agri_TT_24_25.pdf')}}" download>Download PDF</a>
+                </div>
+                <div class="card-pdf">
+                    <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
+                    <p class="description">Time Table (Academic Session 2023-24)</p>
+                    <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12"
+                        href="{{asset('/assets/pdf/time_table/Agriculture_TT_23_24.pdf')}}" download>Download PDF</a>
+                </div>
+                <div class="card-pdf">
+                    <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
+                    <p class="description">Time Table (Academic Session 2022-23)</p>
+                    <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12"
+                        href="{{asset('/assets/pdf/time_table/agriculture_22_23.pdf')}}" download>Download PDF</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
