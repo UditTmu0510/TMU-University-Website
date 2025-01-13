8cfab9762c58811735da3b1da0ce7da5acce63e5
MathuR-A01 <mathuraayush037@gmail.com>
2025-01-13 17:20:34 +0530

    Added the acdemic calendar for the session 24-25 into the directory

--- resources/views/university/colleges/ccsit/ccsit_academic_calendar.blade.php
+++ resources/views/university/colleges/ccsit/ccsit_academic_calendar.blade.php
@@ -2,19 +2,27 @@
 @section('content')
 
 <div class="main-content">
-            <div class="container d-none d-sm-block">
-                <div style="text-align:center">
-                <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Academic</span><span> Calendar</span></h1>
-                <iframe src="{{asset('assets/pdf/ccsit/University-Academic-Calendar-2023-24.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
-                </div>
-            </div>
-            <div class="container d-block d-sm-none mb-3">
-                <div style="text-align:center">
-                <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Academic</span><span> Calendar</span></h1>
-                <a href="{{asset('assets/pdf/ccsit/University-Academic-Calendar-2023-24.pdf')}}" class="tmu-btn btn-4" download>Download PDF</a>
+    <div class="container d-none d-sm-block">
+        <div style="text-align:center">
+            <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Academic</span><span> Calendar</span></h1>
+            <iframe src="{{asset('assets/pdf/academic_calendars/ccsit/TMU-Academic-Calendar-2024-25-R.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
+        </div>
+    </div>
+    <div class="container d-block d-sm-none mb-3">
+        <div style="text-align:center">
+        <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Academic</span><span> Calendar</span></h1>
+            <div class="iqac-container">
+                <div class="iqac-card">
+                    <div class="card-pdf">
+                        <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
+                        <p class="description">Academic Year <br><b>2024 - 25</b></p>
+                        <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12" href="{{asset('assets/pdf/academic_calendars/ccsit/TMU-Academic-Calendar-2024-25-R.pdf')}}">Download PDF</a>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 </div>
 
 @endsection
