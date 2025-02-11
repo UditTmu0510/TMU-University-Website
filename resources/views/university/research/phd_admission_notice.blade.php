24648dc385b112dca0f0cc6ffc33d99fe452114d
MathuR-A01 <mathuraayush037@gmail.com>
2025-02-11 13:33:27 +0530

    replaced the result data and inserted them into the directory

--- resources/views/university/research/phd_admission_notice.blade.php
+++ resources/views/university/research/phd_admission_notice.blade.php
@@ -10,15 +10,21 @@
                 <span> Ph.D. Programme</span>
             </h1>
 
-            <div class="col-md-12 mb-3">
+            <div class="col-md-6 mb-3">
+                <img class="card-img-top" src="{{asset('/assets/img/research/admission-notice/Ph.D-ADVT-2025-law.jpg')}}"
+                    alt="Card image cap" style="height: 700px;">
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="col-md-6 mb-3">
                 <img class="card-img-top" src="{{asset('/assets/img/research/admission-notice/PhD-ADVT-2025-decemebr.jpg')}}"
-                    alt="Card image cap">
+                    alt="Card image cap" style="height: 700px;">
                 <a href="#!">
                     <div class="mask rgba-white-slight"></div>
                 </a>
             </div>
 
-            <!-- container ends -->
         </div>
 
     </div>
