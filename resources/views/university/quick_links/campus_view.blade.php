a5f6dbabd8d48c7603c309ac302c0e250a60ebb7
Ug0510 <uditgg0510@gmail.com>
2025-02-12 11:23:18 +0530

    Completed Mobile and Tablet view of Faculty Accomodation page

--- resources/views/university/quick_links/campus_view.blade.php
+++ resources/views/university/quick_links/campus_view.blade.php
@@ -13,9 +13,6 @@
                         <div class="content-overlay">
                             <div class="container">
                                 <h1 class="section-title d-none d-lg-block" style="color: white;">CAMPUS <span>VIEW</span></h1>
-                                <p class="lead mb-4 col-md-6 d-none d-lg-block">Discover our state-of-the-art facilities and beautiful campus
-                                    environment that provides the perfect setting for academic excellence and personal
-                                    growth.</p>
                                 <div class="stat-box d-none d-lg-inline-block">
                                     <div class="row">
                                         <div class="col-md-4">
