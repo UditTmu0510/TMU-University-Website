ba779b09215e6cc031429d189dd27bc421cddece
MathuR-A01 <mathuraayush037@gmail.com>
2025-02-12 12:57:02 +0530

    Added the anti ragging of the recent year

--- resources/views/university/colleges/physiotherapy/physiotherapy_anti_ragging.blade.php
+++ resources/views/university/colleges/physiotherapy/physiotherapy_anti_ragging.blade.php
@@ -4,7 +4,7 @@
 <div class="main-content">
 
 
-    <h1 class="tmu-text-primary tmu-page-heading"><span>Anti Ragging </span><span> Committee</span></h1>
+    <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Anti Ragging </span><span> Committee</span></h1>
 
     <div class="container">
         <div class="row mb-3">
@@ -20,28 +20,28 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <td class="text-center">1</td>
+                            <td class="text-center">1.</td>
                             <td class="text-center">Ms. Shivanee M. Kaul</td>
                             <td class="text-center">Chairperson</td>
                             <td class="text-center">9568232444</td>
                         </tr>
 
                         <tr>
-                            <td class="text-center">2</td>
-                            <td class="text-center">Mr. Farhan Khan</td>
+                            <td class="text-center">2.</td>
+                            <td class="text-center">Mr. Harish Sharma</td>
                             <td class="text-center">Proctor</td>
-                            <td class="text-center">9793099084</td>
+                            <td class="text-center">9528319985</td>
                         </tr>
 
                         <tr>
-                            <td class="text-center">3</td>
-                            <td class="text-center">Ms. Komal Nagar</td>
+                            <td class="text-center">3.</td>
+                            <td class="text-center">Ms. Priya Sharma</td>
                             <td class="text-center">Member</td>
-                            <td class="text-center">8218532272</td>
+                            <td class="text-center">8527872293</td>
                         </tr>
 
                         <tr>
-                            <td class="text-center">4</td>
+                            <td class="text-center">4.</td>
                             <td class="text-center">Ms. Samarpita Senapati</td>
                             <td class="text-center">Member Secretary & FIP</td>
                             <td class="text-center">6383218957</td>
@@ -56,7 +56,7 @@
                 <div class="col-md-8 mb-3 mb-sm-0" style="background-color: #001055;">
                     <div class="card border-light my-3">
                         <div class="card-body text-center">
-                            <h3 class="card-title tmu-text-primary display-6"><span>ANTI RAGGING TOLL FREE</span></h3>
+                            <h4 class="card-title tmu-text-primary display-6"><span>ANTI RAGGING TOLL FREE</span></h4>
                             <p class="card-text h1 tmu-color">24X7 HELPLINE NO. 1800-180-5522 </p>
                         </div>
                     </div>
@@ -78,25 +78,31 @@
                     </thead>
                     <tbody>
                         <tr>
-                        <tr>
-                            <td class="text-center">1</td>
-                            <td class="text-center">Mr. Farhan Khan</td>
+                            <td class="text-center">1.</td>
+                            <td class="text-center">Mr. Harish Sharma</td>
                             <td class="text-center">Proctor</td>
-                            <td class="text-center">9793099084</td>
+                            <td class="text-center">9528319985</td>
                         </tr>
 
                         <tr>
-                            <td class="text-center">2</td>
-                            <td class="text-center">Ms. Komal Nagar</td>
+                            <td class="text-center">2.</td>
+                            <td class="text-center">Mr. Nand Kishore </td>
                             <td class="text-center">Member</td>
-                            <td class="text-center">8218532272</td>
+                            <td class="text-center">8979307065</td>
                         </tr>
 
                         <tr>
-                            <td class="text-center">3</td>
-                            <td class="text-center">Ms. Priya Sharma </td>
+                            <td class="text-center">3.</td>
+                            <td class="text-center">Ms. Shipra</td>
                             <td class="text-center">Member</td>
-                            <td class="text-center">8527872293</td>
+                            <td class="text-center">9711592146</td>
+                        </tr>
+
+                        <tr>
+                            <td class="text-center">4.</td>
+                            <td class="text-center">Ms. Samarpita Senapati</td>
+                            <td class="text-center">Member</td>
+                            <td class="text-center">6383218957</td>
                         </tr>
                     </tbody>
                 </table>
