a15136a9a3d9b17be15ad22c0d09a2a220ba7de6
Ug0510 <85778092+Ug0510@users.noreply.github.com>
2025-02-13 11:04:56 +0530

    Update sports.blade.php

--- resources/views/university/quick_links/sports.blade.php
+++ resources/views/university/quick_links/sports.blade.php
@@ -78,7 +78,7 @@
             <div class="container">
                 <div class="gallery-header">
                 <h2 Class="tmu-text-primary mb-2 mt-5"><span>Sports </span><span>Gallery</span></h2>
-                    <p class="lead">Explore our beautiful campus through these captivating images</p>
+                    <p class="lead">Explore our stadiums through these captivating images</p>
                 </div>
 
                 <div class="gallery-filters">
