8ba87076cbdda9c61e6972c2daa5124c8e05622c
MathuR-A01 <mathuraayush037@gmail.com>
2025-02-14 12:30:00 +0530

    Added the pdf strucrure itno the page

--- resources/views/university/nep/nep_multidisciplinary_specializations.blade.php
+++ resources/views/university/nep/nep_multidisciplinary_specializations.blade.php
@@ -1,20 +1,30 @@
 @extends('layouts.university.departments.nep_with_sidebar')
 @section('content')
+
 <div class="main-content">
     <div class="container d-none d-sm-block">
         <div style="text-align:center">
             <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Multidisciplinary </span><span> Specializations</span></h1>
-            <iframe src="{{asset('/assets/pdf/research/NEP-2020/Updated-Multidisciplinary-Minor-Specialization.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
+            <iframe src="{{asset('/assets/pdf/research/NEP-2020/Multidisciplinary-Minor-Courses-2024-25.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
         </div>
     </div>
     <div class="container d-block d-sm-none mb-3">
         <div style="text-align:center">
             <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Multidisciplinary </span><span> Specializations</span></h1>
-            <a href="{{asset('/assets/pdf/research/NEP-2020/Updated-Multidisciplinary-Minor-Specialization.pdf')}}" class="tmu-btn btn-4" download>Download PDF</a>
+        </div>
+
+        <div class="iqac-container">
+            <div class="iqac-card">
+                <div class="card-pdf">
+                    <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
+                    <p class="description">NEP 2020 Multidisciplinary Course</p>
+                    <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12"
+                        href="{{asset('/assets/pdf/research/NEP-2020/Multidisciplinary-Minor-Courses-2024-25.pdf')}}" download>Download PDF</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 </div>
-<!-- Bootstrap JS and dependencies -->
 
 @endsection
