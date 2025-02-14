849b8bea66ee9a2179118461d18c82e9e8fafec4
Samkit Modi <sammodtab@gmail.com>
2025-02-14 17:19:43 +0530

    changed the photo and heading

--- resources/views/university/quick_links/rdc_faculty_profile.blade.php
+++ resources/views/university/quick_links/rdc_faculty_profile.blade.php
@@ -6,10 +6,10 @@
   <div class="container">
     <!-- Write content for right side here  -->
     <div class="row mx-0">
-      <h1 class=" tmu-text-primary tmu-page-heading mb-0 pt-md-5 pb-4"><span>TMU Research:  </span><span> Faculty Profiles & Expertise</span></h1>
+      <h1 class=" tmu-text-primary tmu-page-heading mb-0 pt-md-5 pb-4"><span>RDC </span><span> Faculty Profiles</span></h1>
       <div class="col-sm-6 col-lg-4 col-xl-4 col-md-6 col-xs-12">
         <div class="card card-animation">
-          <img src="{{asset('assets/img/department/rdc/souvik_sur.jpg')}}" class="card-img-top" alt="Image 1" >
+          <img src="{{asset('assets/img/department/rdc/souvik_sur1.jpg')}}" class="card-img-top" alt="Image 1" >
           <div class="card-body text-center"> <!-- Added 'text-center' class -->
             <h5 class="card-title tmu-color fw-bold">Dr. Souvik Sur</h5>
             <p class="card-text tmu-color-blue">Assistant Professor</p>
