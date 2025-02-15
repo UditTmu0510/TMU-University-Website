74dde1d4401debb9a2c13fd2c085711ca8683425
MathuR-A01 <mathuraayush037@gmail.com>
2025-02-15 16:05:16 +0530

    Added the placement calednar into the directory

--- resources/views/university/colleges/engineering/engineering_placement_calendar.blade.php
+++ resources/views/university/colleges/engineering/engineering_placement_calendar.blade.php
@@ -6,118 +6,23 @@
         <div class="text-content">
             <h1 class="tmu-text-primary tmu-page-heading pt-md-5"><span>Placement </span><span> Calendar</span></h1>
         </div>
-
         <p class="text-justify mb-4">Training & Placement Cell of Faculty of Engineering endeavor to follow set of Training & Placement norm round the year. A Placement calendar provides hands of placement activities information accompanied with the academics.</p>
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped mb-3">
-                <thead>
-                    <tr>
-                        <th class="text-center">Sl. No.</th>
-                        <th>Targets</th>
-                        <th>Targets Month</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="text-center">1</td>
-                        <td> July</td>
-                        <td>
-                            <ul class="m-ol-list ps-5">
-                                <li> Analysis of placement statistics for passed out batches.</li>
-                                <li> Identification of areas of Improvement and training requirements.</li>
-                                <li> Preparation of Placement Calendar.</li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-center">2</td>
-                        <td>July- August</td>
-                        <td>
-                            <ul class="m-ol-list ps-5">
-                                <li> Fixation of placement drives tentative schedules for passing out batches of all colleges.</li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-center">3</td>
-                        <td>August -September </td>
-                        <td>
-                            <ul class="m-ol-list ps-5">
-                                <li> Website updation on placement data.</li>
-                                <li> Sending invitations to corporate for placement drive.</li>
-                                <li> Student Enrollment on ERP.</li>
-                                <li> Boot Camp Execution.</li>
-                                <li> Resume Building.</li>
-                                <li> Guest / Expert Lectures plan and execution of one lecture per course.</li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-center">4</td>
-                        <td>October</td>
-                        <td>
-                            <ul class="m-ol-list ps-5">
-                                <li> Industry Visit for Engineering branches and basic Sciences.</li>
-                                <li> Industry Visit for Pharmacy Courses.</li>
-                                <li> Expert Lectures as per the plan.</li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-center">5</td>
-                        <td>November</td>
-                        <td>
-                            <ul class="m-ol-list ps-5"
-                                <li> Review of training plan with CTLD.</li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-center">6</td>
-                        <td>December
-                        </td>
-                        <td>
-                            <ul class="m-ol-list ps-5">
-                                <li> Preparation for next phase of placement drives starting from January.</li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-center">7</td>
-                        <td>January –March</td>
-                        <td>
-                            <ul class="m-ol-list ps-5">
-                                <li> Conducting HR Conclave.</li>
-                                <li> Extensive industry visit especially companies in Delhi/NCR and other job industry zones. </li>
-                                <li> Explore possibilities of summer internships, live projects and work assignments for pre &nbsp&nbspfinal year students.</li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-center">8</td>
-                        <td>January-May</td>
-                        <td>
-                            <ul class="m-ol-list ps-5">
-                                <li>Conducting campus recruitment drives focusing on high package companies for students.</li>
-                                <li>Organizing remaining Expert Lectures.</li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-center">9</td>
-                        <td>Year Round Activities
-                        </td>
-                        <td>
-                            <ul class="m-ol-list ps-5">
-                                <li> Offer letter / Appointment Letters distribution by companies- for Placed Batch.</li>
-                                <li> Thanks letters to Companies and sharing of feedback.</li>
-                                <li> Database Updating for next year placements.</li>
-                                <li> Joining of Students.</li>
-                            </ul>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="container d-none d-sm-block">
+            <div style="text-align:center">
+                <iframe src="{{asset('/assets/pdf/placement_calendar/engineering/Placement-Calendar-Session 2024-25.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
+            </div>
+        </div>
+        <div class="container d-block d-sm-none mb-3">
+            <div class="iqac-container">
+                <div class="iqac-card">
+                    <div class="card-pdf">
+                        <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
+                        <p class="description">Placement Calendar (2024-25)</p>
+                        <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12"
+                            href="{{asset('/assets/pdf/placement_calendar/engineering/Placement-Calendar-Session 2024-25.pdf')}}" download>Download PDF</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
