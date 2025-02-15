d3fa7dc94c76907f0b0b73ca19a224e420b768b3
Samkit Modi <sammodtab@gmail.com>
2025-02-15 17:22:54 +0530

    removed the 2024 data from site

--- resources/views/university/nirf/nirf.blade.php
+++ resources/views/university/nirf/nirf.blade.php
@@ -140,48 +140,6 @@
         </div>
     </div>
 
-    <div class="row mb-4">
-        <div class="container d-none d-sm-block">
-            <div style="text-align:center">
-                <h3 class="tmu-text-primary tmu-page-heading mt-md-5" id="overall"><span>NIRF 2024</span><span> Overall Data</span></h3>
-                <iframe src="{{asset('/assets/pdf/nirf/2024/TMU_NIRF_2024_Overall.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
-            </div>
-        </div>
-        <div class="container d-block d-sm-none mb-3">
-            <div style="text-align:center">
-                <h3 class="tmu-text-primary tmu-page-heading mt-md-5" id="overall"><span>NIRF 2024 </span><span> Overall Data</span></h3>
-                <a href="{{asset('/assets/pdf/nirf/2024/TMU_NIRF_2024_Overall.pdf')}}" class="tmu-btn btn-4" download>Download PDF</a>
-            </div>
-        </div>
-    </div>
-    <div class="row mb-4">
-        <div class="container d-none d-sm-block">
-            <div style="text-align:center">
-                <h3 class="tmu-text-primary tmu-page-heading mt-md-5" id="medical"><span>NIRF 2024</span><span> Medical Data</span></h3>
-                <iframe src="{{asset('/assets/pdf/nirf/2024/TMU_NIRF_2024_Medical.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
-            </div>
-        </div>
-        <div class="container d-block d-sm-none mb-3">
-            <div style="text-align:center">
-                <h3 class="tmu-text-primary tmu-page-heading mt-md-5" id="medical"><span>NIRF 2024 </span><span> Medical Data</span></h3>
-                <a href="{{asset('/assets/pdf/nirf/2024/TMU_NIRF_2024_Medical.pdf')}}" class="tmu-btn btn-4" download>Download PDF</a>
-            </div>
-        </div>
-    </div>
-    <div class="row mb-4">
-        <div class="container d-none d-sm-block">
-            <div style="text-align:center">
-                <h3 class="tmu-text-primary tmu-page-heading mt-md-5" id="dental"><span>NIRF 2024</span><span> Dental Data</span></h3>
-                <iframe src="{{asset('/assets/pdf/nirf/2024/TMU_NIRF_2024_Dental.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
-            </div>
-        </div>
-        <div class="container d-block d-sm-none mb-3">
-            <div style="text-align:center">
-                <h3 class="tmu-text-primary tmu-page-heading mt-md-5" id="dental"><span>NIRF 2024 </span><span> Dental Data </span></h3>
-                <a href="{{asset('/assets/pdf/nirf/2024/TMU_NIRF_2024_Dental.pdf')}}" class="tmu-btn btn-4" download>Download PDF</a>
-            </div>
-        </div>
-    </div>
 </div>
 </div>
 
