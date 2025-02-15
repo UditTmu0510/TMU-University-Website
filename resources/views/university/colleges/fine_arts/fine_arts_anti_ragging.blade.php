556b4fb4172190a0f46fff0ca4c0369da9ba22ea
MathuR-A01 <mathuraayush037@gmail.com>
2025-02-15 17:13:34 +0530

    Added the anti ragging committee into the finearts directory

--- resources/views/university/colleges/fine_arts/fine_arts_anti_ragging.blade.php
+++ resources/views/university/colleges/fine_arts/fine_arts_anti_ragging.blade.php
@@ -2,125 +2,125 @@
 @section('content')
 
 
-        <div class="main-content">  
-            <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Anti Ragging </span><span> Committee</span></h1>
+<div class="main-content">
+    <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Anti Ragging </span><span> Committee</span></h1>
 
-                    <div class="container">
-                        <div class="row mb-3">
-                            <div class="table-responsive">
-                                <table class="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">Sl. No.</th>
-                                            <th class="text-center">Name</th>
-                                            <th class="text-center">Designation</th>
-                                            <th class="text-center">Contact No.</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                       <tr>
-										<td class="text-center">1</td>
-										<td class="text-center">Mr. Ravindra Dev</td>
-										<td class="text-center">Principal</td>
-										<td class="text-center">9411970851</td>
+    <div class="container">
+        <div class="row mb-3">
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th class="text-center">Sl. No.</th>
+                            <th class="text-center">Name</th>
+                            <th class="text-center">Designation</th>
+                            <th class="text-center">Contact No.</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="text-center">1</td>
+                            <td class="text-center">Mr. Ravindra Dev</td>
+                            <td class="text-center">Chairperson</td>
+                            <td class="text-center">9411970851</td>
 
-									</tr>
+                        </tr>
 
-									<tr>
-										<td class="text-center">2</td>
-										<td class="text-center">Dr. Farha Deeba</td>
-										<td class="text-center">Assistant Professor </td>
-										<td class="text-center">9045395032</td>
+                        <tr>
+                            <td class="text-center">2</td>
+                            <td class="text-center">Dr. Farha Deeba</td>
+                            <td class="text-center">Proctor</td>
+                            <td class="text-center">9045395032</td>
 
-									</tr>
+                        </tr>
 
-									<tr>
-										<td class="text-center">3</td>
-										<td class="text-center">Mr. Ankur Dev</td>
-										<td class="text-center">Assistant Professor </td>
-										<td class="text-center">9837435792</td>
+                        <tr>
+                            <td class="text-center">3</td>
+                            <td class="text-center">Mr. Ankur Dev</td>
+                            <td class="text-center">Member</td>
+                            <td class="text-center">9837435792</td>
 
-									</tr>
+                        </tr>
 
-									<tr>
-										<td class="text-center">4</td>
-										<td class="text-center">Mr. Pravesh Chandra Verma</td>
-										<td class="text-center">Assistant Professor </td>
-										<td class="text-center">9336150676</td>
+                        <tr>
+                            <td class="text-center">4</td>
+                            <td class="text-center">Mr. Pravesh Chandra Verma</td>
+                            <td class="text-center">First Information Person</td>
+                            <td class="text-center">9336150676</td>
 
-									</tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
-                        <div class="container">
-                            <div class="row justify-content-center mb-5 mt-5">
-                                <div class="col-md-8 mb-3 mb-sm-0" style="background-color: #001055;">
-                                    <div class="card border-light my-3">
-                                        <div class="card-body text-center">
-                                            <h3 class="card-title tmu-text-primary display-6"><span>ANTI RAGGING TOLL FREE</span></h3>
-                                            <p class="card-text h1 tmu-color">HELPLINE NO. 1800-180-5522 </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <h2 class="tmu-text-primary tmu-page-heading"><span> Proctorial </span><span>Board</span></h2>
-                            <div class="table-responsive">
-                                <table class="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">Sl. No. </th>
-                                            <th class="text-center">Name</th>
-                                            <th class="text-center">Designation</th>
-                                            <th class="text-center">Status</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                       <tr>
-										<td class="text-center">1</td>
-										<td class="text-center">Mr. Ravindra Dev</td>
-										<td class="text-center">Chairperson</td>
-										<td class="text-center">9411970851</td>
-									</tr>
-
-									<tr>
-										<td class="text-center">2</td>
-										<td class="text-center">Dr. Farha Deeba</td>
-										<td class="text-center">Member</td>
-										<td class="text-center">9045395032</td>
-									</tr>
-
-									<tr>
-										<td class="text-center">3</td>
-										<td class="text-center">Mr. Anup Kumar Pandey</td>
-										<td class="text-center">Member</td>
-										<td class="text-center">9873362133</td>
-									</tr>
-
-									<tr>
-										<td class="text-center">4</td>
-										<td class="text-center">Mr. Rajesh Kumar</td>
-										<td class="text-center">First Information Person-I</td>
-										<td class="text-center">9971913176</td>
-									</tr>
-
-									<tr>
-										<td class="text-center">5</td>
-										<td class="text-center">Mr. Vaibhav Jha</td>
-										<td class="text-center">First Information Person-II</td>
-										<td class="text-center">7523906969</td>
-
-									</tr>
-                                    </tbody>
-                                </table>
-                            </div>
+        <div class="container">
+            <div class="row justify-content-center mb-5 mt-5">
+                <div class="col-md-8 mb-3 mb-sm-0" style="background-color: #001055;">
+                    <div class="card border-light my-3">
+                        <div class="card-body text-center">
+                            <h3 class="card-title tmu-text-primary display-6"><span>ANTI RAGGING TOLL FREE</span></h3>
+                            <p class="card-text h1 tmu-color">24X7 HELPLINE NO. 1800-180-5522 </p>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <h2 class="tmu-text-primary tmu-page-heading"><span> Proctorial </span><span>Board</span></h2>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th class="text-center">Sl. No. </th>
+                            <th class="text-center">Name</th>
+                            <th class="text-center">Designation</th>
+                            <th class="text-center">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="text-center">1</td>
+                            <td>Mr. Ravindra Dev</td>
+
+                            <td style="text-align:center;">Chairperson</td>
+                            <td class="text-center">9411970851</td>
+
+                        </tr>
+
+                        <tr>
+                            <td class="text-center">2</td>
+                            <td>Dr. Farha Deeba</td>
+
+                            <td style="text-align:center;">Proctor</td>
+                            <td class="text-center">9045395032</td>
+
+                        </tr>
+
+                        <tr>
+                            <td class="text-center">3</td>
+                            <td>Mr. Ankur Dev</td>
+
+                            <td style="text-align:center;">Member</td>
+                            <td class="text-center">9837435792</td>
+
+                        </tr>
+
+                        <tr>
+                            <td class="text-center">4</td>
+                            <td>Mr. Vaibhav Jha</td>
+
+                            <td style="text-align:center;">Member Secretary & FIP</td>
+                            <td class="text-center">7523906969</td>
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
+</div>
+</div>
 
-    @endsection
+@endsection
