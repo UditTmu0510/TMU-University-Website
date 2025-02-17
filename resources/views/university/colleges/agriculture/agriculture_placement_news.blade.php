6e4b60f744ef053b2d9f60f6576a08afa7f7aafc
MathuR-A01 <mathuraayush037@gmail.com>
2025-02-17 14:08:07 +0530

    Added the AC and placement news into the page

--- resources/views/university/colleges/agriculture/agriculture_placement_news.blade.php
+++ resources/views/university/colleges/agriculture/agriculture_placement_news.blade.php
@@ -6,14 +6,57 @@
         <h1 class="tmu-text-primary tmu-page-heading"><span>Placement </span><span> News </span></h1>
         <div class="container">
             <div class="row mb-3">
-                <div class="table-responsive">
-                    <table class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th class="text-center fw-bold"> Data Will be updated Soon! </th>
-                            </tr>
-                        </thead>
-                    </table>
+                <div class="col-lg-4 col-md-6 col-sm-6 mb-3">
+                    <div class="card">
+                        <div class="bg-image hover-overlay ripple rounded-0"
+                            data-mdb-ripple-color="light">
+                            <img class="img-fluid" src="{{asset('/assets/img/placement_news/agriculture/1.jpg')}}"
+                                alt="Card image cap" />
+                            <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);">
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 mb-3">
+                    <div class="card">
+                        <div class="bg-image hover-overlay ripple rounded-0"
+                            data-mdb-ripple-color="light">
+                            <img class="img-fluid" src="{{asset('/assets/img/placement_news/agriculture/2.jpg')}}"
+                                alt="Card image cap" />
+                            <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);">
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 mb-3">
+                    <div class="card">
+                        <div class="bg-image hover-overlay ripple rounded-0"
+                            data-mdb-ripple-color="light">
+                            <img class="img-fluid" src="{{asset('/assets/img/placement_news/agriculture/3.jpg')}}"
+                                alt="Card image cap" />
+                            <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);">
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 mb-3">
+                    <div class="card">
+                        <div class="bg-image hover-overlay ripple rounded-0"
+                            data-mdb-ripple-color="light">
+                            <img class="img-fluid" src="{{asset('/assets/img/placement_news/agriculture/4.jpg')}}"
+                                alt="Card image cap" />
+                            <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);">
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
