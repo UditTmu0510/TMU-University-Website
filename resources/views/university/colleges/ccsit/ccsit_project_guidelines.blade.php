f3faa3be5231268bca35c8a9f1a3823889b216bb
Samkit Modi <sammodtab@gmail.com>
2025-02-18 13:43:14 +0530

    changed the card design for mobile view

--- resources/views/university/colleges/ccsit/ccsit_project_guidelines.blade.php
+++ resources/views/university/colleges/ccsit/ccsit_project_guidelines.blade.php
@@ -3,7 +3,7 @@
 
 
 <div class="main-content">
-    <div class="container">
+    <!-- <div class="container">
         <h1 class="tmu-text-primary tmu-page-heading"><span>Project</span><span> Guidlines</span></h1>
 
         <div class="container">
@@ -19,6 +19,26 @@
                                 aria-hidden="true"></i>Click here to
                             download PDF file.</a></center>
                 </object>
+            </div>
+        </div>
+    </div> -->
+    <div class="container d-none d-sm-block">
+        <div style="text-align:center">
+            <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Project</span><span> Guidlines</span></h1>
+            <iframe src="{{asset('assets/pdf/ccsit/new/G001-TMU-Project-Guidelines-v5.2.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
+        </div>
+    </div>
+    <div class="container d-block d-sm-none mb-3">
+        <div style="text-align:center">
+        <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Project</span><span> Guidlines</span></h1>
+            <div class="iqac-container">
+                <div class="iqac-card">
+                    <div class="card-pdf">
+                        <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
+                        <p class="description"><b>Project <br>Guidelines</b></p>
+                        <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12" href="{{asset('assets/pdf/ccsit/new/G001-TMU-Project-Guidelines-v5.2.pdf')}}">Download PDF</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
