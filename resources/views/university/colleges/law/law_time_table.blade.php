a129b769d65092f65ed14d43e309f13ac8bb7a10
MathuR-A01 <mathuraayush037@gmail.com>
2025-02-19 15:55:44 +0530

    Added the time table and updated heading on the page

--- resources/views/university/colleges/law/law_time_table.blade.php
+++ resources/views/university/colleges/law/law_time_table.blade.php
@@ -1,20 +1,27 @@
 @extends('layouts.university.colleges.law_with_sidebar')
 @section('content')
-        <div class="main-content">
-            <div class="container d-none d-sm-block">
-                <div style="text-align:center">
-                <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Time </span><span > Table</span></h1>
-                <iframe src="{{asset('/assets/pdf/pdf/law_TT_Odd_sem_23_24.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
-                </div>
-            </div>
-            <div class="container d-block d-sm-none mb-3">
-                <div style="text-align:center">
-                <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Time </span><span > Table</span></h1>
-                <a href="{{asset('/assets/pdf/pdf/law_TT_Odd_sem_23_24.pdf')}}" class="tmu-btn btn-4" download>Download PDF</a>
-                
+    <div class="main-content">
+    <div class="container d-none d-sm-block">
+        <div style="text-align:center">
+            <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Time </span><span > Table</span></h1>
+            <iframe src="{{asset('/assets/pdf/time_table/law/TMCLLS-TT-Even2024-25.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
+        </div>
+    </div>
+    <div class="container d-block d-sm-none mb-3">
+        <div style="text-align:center">
+            <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Time </span><span > Table</span></h1>
+            <div class="iqac-container">
+                <div class="iqac-card">
+                    <div class="card-pdf">
+                        <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
+                        <p class="description">Time Table Even Semester <br><b>2024 - 25</b></p>
+                        <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12" href="{{asset('assets/pdf/time_table/law/TMCLLS-TT-Even2024-25.pdf')}}">Download PDF</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
 <!-- Bootstrap JS and dependencies -->
 @endsection
