6f19c74d9bd78cbcff38e2eb1cbf0ac276556c0c
Samkit Modi <sammodtab@gmail.com>
2025-02-22 17:52:34 +0530

    made changes to the fine arts college pages accordig to the review

--- resources/views/university/colleges/fine_arts/fine_arts_overview.blade.php
+++ resources/views/university/colleges/fine_arts/fine_arts_overview.blade.php
@@ -23,17 +23,17 @@
                             <p class="bq-title snipcss0-2-5-6 text-justify " >The term "Fine Arts" refers to art forms developed primarily for their aesthetic value and beauty.</p>
                         </blockquote>
                         <h3 class="snipcss0-1-1-7 style-csfwy" id="style-csfwy">What Does Fine Arts Include?</h3>
-                        <h4 class="snipcss0-1-1-8 style-p78Ac" id="style-p78Ac">The main fine arts include film, dance, painting, photography, architecture, pottery, conceptual art, sculpture, music, printmaking, interior design, and drama.</h4>
+                        <h4 class="snipcss0-1-1-8 style-p78Ac" id="style-p78Ac">The main fine arts include painting, photography, pottery, conceptual art, sculpture, and printmaking.</h4>
                         <blockquote class="blockquote bq-success snipcss0-1-1-9 ">
                             <p class="bq-title snipcss0-2-9-10 text-justify ">In simple terms,</p>
-                            <p class="text-justify snipcss0-2-9-10 ">Fine Arts is the study and creation of visual art forms. It can take the shape of dance, painting, photography, film, architecture, and more.</p>
+                            <p class="text-justify snipcss0-2-9-10 ">Fine Arts is the study and creation of visual art forms. It can take the shape of painting, photography, and more.</p>
                         </blockquote>
                     </div>
                 </div>
                 <div class="row mt-5">
                     <p class="mb-3">The College of Fine Arts, established in 2011, offers Bachelor’s and Master’s programmes in Fine Arts, building on historical perspectives of art and design, with an emphasis on understanding and addressing contemporary needs while planning and preparing for the future. The objective of these programmes is to provide appreciation and training in diverse arts through modern concepts and methods of art education. Fine Arts encompass all aspects of nature and human creativity in the pursuit of substance, form, and expression. The programmes educate and prepare students to become artists and to engage in practices aligned with the creation of art.</p>
                     <p class="mb-3">The College of Fine Arts fosters a vibrant culture that supports cutting-edge practice and research. Students are welcomed into an environment that reflects the openness and diversity of contemporary art and culture. Faculty members are committed to integrating new technologies with traditional media and historical fine arts practices.</p>
-                    <p class="mb-3">By taking advantage of trend-setting academic programmes and research opportunities, students thrive in an atmosphere of exploration and discovery, developing as practicing artists. Graduates from the college work as Designers, Painters, Sculptors, Photographers, Creative Directors, Illustrators, Portrait Artists, and Textile Designers with leading fashion brands.</p>
+                    <p class="mb-3">By taking advantage of trend-setting academic programmes and research opportunities, students thrive in an atmosphere of exploration and discovery, developing as practicing artists. Graduates from the college work as Designers, Painters, Sculptors, Photographers, Creative Directors, Illustrators, and Portrait Artists with leading fashion brands.</p>
                 </div>
             </div>
         </div>
