d179ae0472afc71d423752800779fc6fd7dc4746
Ug0510 <uditgg0510@gmail.com>
2025-02-27 17:46:52 +0530

    Change location of dash.min.js script

--- resources/views/university/header_home.blade.php
+++ resources/views/university/header_home.blade.php
@@ -347,6 +347,8 @@
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800&display=swap"
         rel="stylesheet"> -->
 
+        <script src="{{ asset('assets/js/default/dash.min.js') }}" defer></script>
+
         <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -733,7 +735,6 @@
 
 
         {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/dashjs/4.7.4/dash.all.min.js"></script> --}}
-        <script src="{{asset('assets/js/default/dash.min.js')}}"></script>
 
 
         <link rel="stylesheet" href="{{asset('assets/css/swiper2.css')}}">
