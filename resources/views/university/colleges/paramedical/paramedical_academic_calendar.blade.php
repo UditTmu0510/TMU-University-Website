b6c592fa5795a8023899f5b1de1a989d7c20ef56
MathuR-A01 <mathuraayush037@gmail.com>
2025-02-27 11:02:59 +0530

    Added the / before the assets

--- resources/views/university/colleges/paramedical/paramedical_academic_calendar.blade.php
+++ resources/views/university/colleges/paramedical/paramedical_academic_calendar.blade.php
@@ -5,7 +5,7 @@
     <div class="container d-none d-sm-block">
         <div style="text-align:center">
             <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Academic</span><span> Calendar</span></h1>
-            <iframe src="{{asset('assets/pdf/academic_calendars/paramedical/TMU-Academic-Calendar-2024-25-R.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
+            <iframe src="{{asset('/assets/pdf/academic_calendars/paramedical/TMU-Academic-Calendar-2024-25-R.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
         </div>
     </div>
     <div class="container d-block d-sm-none mb-3">
@@ -16,7 +16,7 @@
                     <div class="card-pdf">
                         <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
                         <p class="description">Academic Year <br><b>2024 - 25</b></p>
-                        <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12" href="{{asset('assets/pdf/academic_calendars/paramedical/TMU-Academic-Calendar-2024-25-R.pdf')}}">Download PDF</a>
+                        <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12" href="{{asset('/assets/pdf/academic_calendars/paramedical/TMU-Academic-Calendar-2024-25-R.pdf')}}">Download PDF</a>
                     </div>
                 </div>
             </div>
