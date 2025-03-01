97c82425ed6e162c84e15eed4d6e1301d7fc8fbc
MathuR-A01 <mathuraayush037@gmail.com>
2025-03-01 10:18:19 +0530

    corrected the spelling of the career into programme header

--- resources/views/university/header_programme_page.blade.php
+++ resources/views/university/header_programme_page.blade.php
@@ -486,7 +486,7 @@
                 <a href="{{route('crc.about.us')}}">Placement</a>
               </div>
               <div class="col-4 mb-3">
-                <a href="{{route('tmu.careers')}}">Carrer</a>
+                <a href="{{route('tmu.careers')}}">Career</a>
               </div>
               <div class="col-4 mb-3">
                 <a href="{{route('all_news')}}">News</a>
