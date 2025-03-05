73d0f75e14bc61350f247ce756201a942f8b6d11
Samkit Modi <sammodtab@gmail.com>
2025-03-05 11:46:05 +0530

    commented the non-working font-awesome

--- resources/views/university/header_programme_page.blade.php
+++ resources/views/university/header_programme_page.blade.php
@@ -385,7 +385,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
 
 
-  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  <!-- <script src="https://kit.fontawesome.com/a076d05399.js"></script> -->
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 
