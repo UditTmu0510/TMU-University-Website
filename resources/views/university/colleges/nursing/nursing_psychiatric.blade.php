c5fa857156742bd8a76ca2b1d4868a991f583836
MathuR-A01 <mathuraayush037@gmail.com>
2025-03-18 11:44:49 +0530

    Remove the useless college overview css

--- resources/views/university/colleges/nursing/nursing_psychiatric.blade.php
+++ resources/views/university/colleges/nursing/nursing_psychiatric.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.university.colleges.nursing_with_sidebar')
 @section('content')
 
-<link rel="stylesheet" href="{{asset('assets/css/college_overview.css')}}">
-
-
     <div class="main-content">
         <div class="container">
 
@@ -13,17 +10,17 @@
                         <h1 class="tmu-text-primary tmu-page-heading"><span>Department of </span><span> Psychiatric Nursing </span></h1>
                         <p class="about-p mb-4">To enable students to become empathetic mental health professional, engaged in promotion of mental health, identification, care and rehabilitation of the mentally ill.</p>
                     </div>
-                    <div class="image-content text-center p-4">
+                    <div class="image-content text-center p-2">
                         <img src="{{asset('/assets/img/college_departments/nursing/psy/banner.jpg')}}" alt="">
                     </div>
                     <div class="container p-3">
-                       <ul class="m-ol-list ps-5 text-justify">
+                       <ul class="m-ol-list ps-3 text-justify">
                            <li>The department offer advanced level of teaching and training to the students.</li>
                            <li>The department undertakes socially relevant and culturally appropriate research.</li>
                            <li>The department provides guidance and counselling services to the students.</li>
                            <li>The department provides specialized clinical exposure to the graduate and post graduate students.</li>
                         </ul>
-                        <p class="about-p mb-4">The department observes significant days, educational and health related programmes, field visits, camp’s in collorabtion with various departments. Standard procedure’s manuals and protocols are in practice and updates as per the current scenario in the changing patterns of health care delivery system and advancement in paediatric care.</p>
+                        <p class="about-p mb-3">The department observes significant days, educational and health related programmes, field visits, camp’s in collorabtion with various departments. Standard procedure’s manuals and protocols are in practice and updates as per the current scenario in the changing patterns of health care delivery system and advancement in paediatric care.</p>
                     </div>
                 </div>
             </section>
