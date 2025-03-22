ab8b0d0ee3a2b795d5f1dd9aaa3fe55155caede5
MathuR-A01 <mathuraayush037@gmail.com>
2025-03-22 14:16:09 +0530

    Added a table data

--- resources/views/university/ciks/activities.blade.php
+++ resources/views/university/ciks/activities.blade.php
@@ -1,5 +1,86 @@
 @extends('layouts.university.departments.ciks_with_sidebar')
 @section('content')
 
+<div class="main-content">
+    <div class="container">
+        <section class="about-section bg-light p-3">
+            <h1 class="tmu-text-primary tmu-page-heading mt-md-5 "><span>Activity Planner: </span><span>  January 2025 - June 2025</span></h1>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <strong>Sl.</strong>
+                                <strong>No.</strong>
+                            </td>
+                            <td>
+                                <strong>Month</strong>
+                            </td>
+                            <td>
+                                <strong>Proposed Activity</strong>
+                            </td>
+                            <td>
+                                <strong>Activity Detail/Theme</strong>
+                            </td>
+                            <td>
+                                <strong>Who can Participate</strong>
+                            </td>
+                            <td>
+                                <strong>Target Audience</strong>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>January</td>
+                            <td>Round The Table Discussion</td>
+                            <td>Enhancing IKS Effectiveness Through Curriculum</td>
+                            <td>Educationists across TMU</td>
+                            <td>Discussants</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>February</td>
+                            <td>FDP</td>
+                            <td>Sanskar-based Education</td>
+                            <td>Eminent Speaker with expertise in IKS</td>
+                            <td>IKS Delegates from all colleges of TMU</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>March</td>
+                            <td>Essay Writing Competition</td>
+                            <td>The Significance of Indian Knowledge Systems in Preserving Cultural Heritage</td>
+                            <td>Competition will be open for all students of the University</td>
+                            <td>All students of the University</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>April</td>
+                            <td>Quiz</td>
+                            <td rowspan="2">IKS Syllabus Based</td>
+                            <td rowspan="2">Students of TMIMT of all Programmes where IKS will be taught as a course either in I or II semester of 2024-25 batch</td>
+                            <td rowspan="2">Students of TMIMT</td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td>May</td>
+                            <td>Exam</td>
+                        </tr>
+                        <tr>
+                            <td>6</td>
+                            <td>June</td>
+                            <td>Expert Talk/ Workshop</td>
+                            <td>Bhagavad-Gita</td>
+                            <td>Eminent Speaker with expertise in Gita Gyan</td>
+                            <td>Faculty of all colleges of TMU
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </div>
+</div>
+</div>
 </div>
 @endsection
--- resources/views/university/ciks/execution.blade.php
+++ resources/views/university/ciks/execution.blade.php
@@ -1,5 +1,93 @@
 @extends('layouts.university.departments.ciks_with_sidebar')
 @section('content')
 
+
+<div class="main-content">
+    <div class="container">
+        <section class="about-section bg-light p-3">
+            <h1 class="tmu-text-primary tmu-page-heading mt-md-5 "><span>Execution of </span><span> Indian Knowledge System</span></h1>
+            <p class="mb-3 fs-14">In line with the National Education Policy 2020, Teerthanker Mahaveer University established the Centre for Indian Knowledge Systems in 2023 as part of various 4 years’ UG programmes, as per the following details:</p>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped">
+                    <thead class="">
+                        <tr>
+                            <th>Sl.No.</th>
+                            <th>College</th>
+                            <th>Programme</th>
+                            <th>Course Name</th>
+                            <th>Course Code</th>
+                            <th>Credits</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td rowspan="3">College of Computing Sciences & IT</td>
+                            <td>BCA</td>
+                            <td>Indian Knowledge System</td>
+                            <td>TMUVA240101</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <td> </td>
+                            <td>B.Sc. (Animation)</td>
+                            <td>Indian Knowledge System</td>
+                            <td>TMUVA240101</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <td> </td>
+                            <td>B.Sc. (Computer Science)</td>
+                            <td>Indian Knowledge System</td>
+                            <td>TMUVA240101</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td rowspan="2">TMIMT, College of Management</td>
+                            <td>BBA</td>
+                            <td>Indian Knowledge System</td>
+                            <td>TMUVA240202</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <td> </td>
+                            <td>B.Com. (Hons.)</td>
+                            <td>Indian Knowledge System</td>
+                            <td>TMUVA240202</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>College of Paramedical Sciences</td>
+                            <td>B.Sc. (Forensic)</td>
+                            <td>Indian Knowledge System</td>
+                            <td>TMUVA240101</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>TMIMT College of Physical Education</td>
+                            <td>BPES</td>
+                            <td>Indian Knowledge System</td>
+                            <td>TMUVA240101</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td>College of Fine Arts</td>
+                            <td>BFA</td>
+                            <td>Indian Knowledge System</td>
+                            <td>TMUVA240101</td>
+                            <td>2</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <p class="mb-2 fs-14 mt-3">The Centre for Indian Knowledge Systems (CIKS) has been actively engaged in promoting education, training, and research in traditional Indian Knowledge Systems. Here are some of the key activities CIKS organized in alignment with its vision.</p>
+        </section>
+    </div>
 </div>
+</div>
+
 @endsection
