2dbcc5894281aa44f43c1bc8cd4cbfcd5660ee00
Samkit Modi <sammodtab@gmail.com>
2025-03-22 11:55:47 +0530

    made controller, routes, pages for ciks

---
+++ app/Http/Controllers/CiksController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CiksController extends Controller
+{
+
+    public function ciks_aboutus()
+    {
+        return view('university.ciks.about_us');
+    }
+
+    public function ciks_patrons()
+    {
+        return view('university.ciks.patrons');
+    }
+
+    public function ciks_team()
+    {
+        return view('university.ciks.team');
+    }
+
+    public function ciks_events()
+    {
+        return view('university.ciks.events');
+    }
+
+    public function ciks_gallery()
+    {
+        return view('university.ciks.gallery');
+    }
+
+    public function ciks_media_coverage()
+    {
+        return view('university.ciks.media_coverage');
+    }
+
+    public function ciks_activities()
+    {
+        return view('university.ciks.activities');
+    }
+
+    public function ciks_execution()
+    {
+        return view('university.ciks.execution');
+    }
+
+}
