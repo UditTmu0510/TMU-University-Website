e7cdd5eccfebdc7ffd4d37c2155aeb04b061ed7d
[Sarvagyachoudhary] <[sarvagya20065@gmail.com]>
2025-03-26 10:52:32 +0530

    changes in the pdf view button on policies and sops 3

--- resources/views/university/iqac/iqac_policies.blade.php
+++ resources/views/university/iqac/iqac_policies.blade.php
@@ -197,7 +197,7 @@
           <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12" href="{{asset('/assets/pdf/policy/sops/TMU_SOP-4-0.pdf')}}" target="_blank">View
             PDF</a>
           <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12" href="{{asset('/assets/pdf/policy/sops/TMU_SOP-4-1.pdf')}}" target="_blank">View
-            PDF</a>
+          Amendement PDF</a>
         </div>
         <div class="card-pdf">
           <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
