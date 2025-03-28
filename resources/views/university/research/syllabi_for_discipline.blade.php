bbada6c68db375a2502bcdd1969566226894be80
MathuR-A01 <mathuraayush037@gmail.com>
2025-03-28 17:26:05 +0530

    closed the b tag

--- resources/views/university/research/syllabi_for_discipline.blade.php
+++ resources/views/university/research/syllabi_for_discipline.blade.php
@@ -162,7 +162,7 @@
                         </div>
                         <div class="card-pdf">
                             <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
-                            <p class="description"><b>Course Name: Modern Radiological Imaging Principles, Techniques & Applications</b><br><b>Course Code - (PDS240112)/b></p>
+                            <p class="description"><b>Course Name: Modern Radiological Imaging Principles, Techniques & Applications</b><br><b>Course Code - (PDS240112)</b></p>
                             <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12"
                                 href="{{asset('/assets/pdf/research/SDSC_fresh/MEDICAL_ALLIED_SCIENCES/PARAMEDICAL/MODERN_RADIOLOGICAL_IMAGING_PRINCIPLES_TECHNIQUES_APPLICATIONS_(PDS240112).pdf')}}" download>Download PDF</a>
                         </div>
