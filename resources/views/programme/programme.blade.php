3096481c9e455bad10b2d6ce940ac5344cc1eeff
Ug0510 <uditgg0510@gmail.com>
2025-04-03 12:47:48 +0530

    improved the padding on form in programme pages

--- resources/views/programme/programme.blade.php
+++ resources/views/programme/programme.blade.php
@@ -236,10 +236,10 @@
     </div>
     <div class="container mt-4">
         <div class="row d-flex">
-            <div class="col-md-8 p-5">
+            <div class="col-md-8">
                 <img src="{{ asset('assets/img/programmes_offered/form-side-banner.png') }}" alt="" class="w-100">
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 pt-5">
                 <h2 class="tmu-text-primary text-center" style="font-size:1.7rem !important; line-height:1.5rem">
                     <span>Apply For Admissions</span><span></span>
                 </h2>
