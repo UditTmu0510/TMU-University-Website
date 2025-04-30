46921302e909eabb7d4f4b41a4d4e45815904de9
MathuR-A01 <mathuraayush037@gmail.com>
2025-04-30 17:56:19 +0530

    added the latest iqac data into the directory

--- resources/views/university/colleges/dental/dental_iqac.blade.php
+++ resources/views/university/colleges/dental/dental_iqac.blade.php
@@ -2,25 +2,27 @@
 @section('content')
 
 <div class="main-content">
-    <div class="container">
-        <h1 class="tmu-text-primary tmu-page-heading"><span>College </span><span> IQAC </span></h1>
-        <div class="container">
-            <div class="row mb-3">
-                <div class="table-responsive">
-                    <table class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th class="text-center fw-bold"> Data Will be updated Soon! </th>
-                            </tr>
-                        </thead>
-                    </table>
+    <div class="container d-none d-sm-block">
+        <div style="text-align:center">
+            <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>College</span><span> IQAC</span></h1>
+            <iframe src="{{asset('assets/pdf/college_iqac/dental/IQAC_COMPLETE_SIG.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
+        </div>
+    </div>
+    <div class="container d-block d-sm-none mb-3">
+        <div style="text-align:center">
+        <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>College</span><span> IQAC</span></h1>
+            <div class="iqac-container">
+                <div class="iqac-card">
+                    <div class="card-pdf">
+                        <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
+                        <p class="description">Academic Year <br><b>2024 - 25</b></p>
+                        <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12" href="{{asset('assets/pdf/college_iqac/dental/IQAC_COMPLETE_SIG.pdf')}}">Download PDF</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<!-- Bootstrap JS and dependencies -->
 </div>
-
 
 @endsection
