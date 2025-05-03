ed91d71ec505265b1873a28dee62d2d9ea4b02f0
MathuR-A01 <mathuraayush037@gmail.com>
2025-05-03 17:24:22 +0530

    added the govt approval latest year data in the directory

--- resources/views/university/colleges/medical/medical_approvals.blade.php
+++ resources/views/university/colleges/medical/medical_approvals.blade.php
@@ -22,6 +22,14 @@
 					<tbody>
 						<tr>
 							<td class="text-center">1</td>
+							<td>(Decision of MARB’s Committee, NMC – Letter of Permission in respect of online application NMC/UGI/2024- 25/000042 , for increase of 100 MBBS Seats (from 150 to 250) at Teerthanker Mahaveer Medical College and Research Centre, Moradabad, U.P for Academic Year (A.Y.): 2024-25-
+								Regarding.)</td>
+							<td>2024-25<br><b>(UPR-2024)</b></td>
+							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/UPR-2024-25.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
+							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/UPR-2024-25.pdf')}}" download="UPR-2024-25.pdf" data-original-title="Download">Download</a></td>
+						</tr>
+						<tr>
+							<td class="text-center">2</td>
 							<td>recognition / Approval of Teerthanker Mahaveer Medical College & Research Centre, Moradabad for the award of MBBS degree granted by Teerthanker Mahaveer University, Moradabad against the increased intake from 100 to 150 seats vide letter No: U.12012/190/2019-ME.I[FTS.8006335] dated 09/05/2019.</td>
 							<td>2019-20<br><b>(UPR-2019)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2019-20_with_recognition.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
@@ -29,7 +37,7 @@
 						</tr>
 
 						<tr>
-							<td class="text-center">2</td>
+							<td class="text-center">3</td>
 							<td>Continuance of recognition of MBBS degree granted by Teerthanker Mahaveer University, in respect of students being trained at Teerthanker Mahaveer Medical College & Research Centre vide letter No: MCI-34(41)(UG)/2018/Med./127287 dated 02/08/2018.</td>
 							<td>2018-19<br><b>(UPR-2018)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/100 seats recoginition renewal.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
@@ -38,77 +46,77 @@
 
 
 						<tr>
-							<td class="text-center">3</td>
+							<td class="text-center">4</td>
 							<td>Renewal of permissions for admission of 5<sup>th</sup> Batch of MBBS students against increased intake from 100-150 at Teerthanker Mahaveer Medical College & Research Centre, Moradabad vide letter No: U.12012/58/2017-ME.I[FTS.3131079] dated 10/01/2018.</td>
 							<td>2018-19<br><b>(UPR-2018)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2018-19.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2018-19.pdf')}}" download="2018-19.pdf" data-original-title="Download">Download</a></td>
 						</tr>
 						<tr>
-							<td class="text-center">4</td>
+							<td class="text-center">5</td>
 							<td>Renewal of permissions for admission of 4<sup>th</sup> Batch of MBBS students against increased intake i.e. from 100-150 at Teerthanker Mahaveer Medical College & Research Centre, Moradabad vide letter No: U.12012/120/2016-ME.I[FTS.3088815] dated 20/02/2017.</td>
 							<td>2017-18<br><b>(UPR-2017)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2017-18.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2017-18.pdf')}}" download="2017-18.pdf" data-original-title="Download">Download</a></td>
 						</tr>
 						<tr>
-							<td class="text-center">5</td>
+							<td class="text-center">6</td>
 							<td>Renewal of permisison for admisison of 3<sup>rd</sup> Batch of MBBS students against the increased intake i.e. from 100-150 at Teerthanker Mahaveer Medical College & Research Centre, Moradabad vide letter No:U.12012/670/2015-ME.I dated 31.12.2015.</td>
 							<td>2016-17<br><b>(UPR-2016)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2016-17.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2016-17.pdf')}}" download="2016-17.pdf" data-original-title="Download">Download</a></td>
 						</tr>
 						<tr>
-							<td class="text-center">6</td>
+							<td class="text-center">7</td>
 							<td>Renewal of permisison for admisison of 2<sup>nd</sup> Batch of MBBS students against the increased intake i.e. from 100-150 vide letter No:U.12012/79/2015-ME.(PII) dated 16.04.2015.</td>
 							<td>2015-16<br><b>(UPR-2015)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2015-16.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2015-16.pdf')}}" download="2015-16.pdf" data-original-title="Download">Download</a></td>
 						</tr>
 						<tr>
-							<td class="text-center">7</td>
+							<td class="text-center">8</td>
 							<td>Increase of Seats in MBBS Course at Teerthanker Mahaveer Medical College & Research Centare, Moradbad u/s 10A of the IMC Act, 1956 vide letter No:U.12012/570/2014-ME.(PII) dated 07.08.2014.</td>
 							<td>2014-15<br><b>(UPR-2014)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2014-15.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2014-15.pdf')}}" download="2014-15.pdf" data-original-title="Download">Download</a></td>
 						</tr>
 						<tr>
-							<td class="text-center">8</td>
+							<td class="text-center">9</td>
 							<td>recognition of 100 Seats in MBBS Course at Teerthanker Mahaveer Medical College & Research Centare, Moradbad vide letter No:U.12012/500/2007-ME.(PII) dated 11.03.2013.</td>
 							<td>2013-14<br><b>(UPR-2013)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2013-14_with_recognition.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2013-14_with_recognition.pdf')}}" download="2013-14_with_recognition.pdf" data-original-title="Download">Download</a></td>
 						</tr>
 						<tr>
-							<td class="text-center">9</td>
+							<td class="text-center">10</td>
 							<td>Renewal of permissioin for admission of 5<sup>th</sup> batch of 100 admissions vide letter No. MCI-34(41)/2012-Med/111712 dated 15/06/2012</td>
 							<td>2012-13<br><b>(UPR-2012)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2012-13.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2012-13.pdf')}}" download="2012-13.pdf" data-original-title="Download">Download</a></td>
 						</tr>
 						<tr>
-							<td class="text-center">10</td>
+							<td class="text-center">11</td>
 							<td>Renewal of permissioin for admission of 4<sup>th</sup> batch of 100 admissions vide letter No. MCI-34(41)/2011-Med/16497 dated 08/06/2011</td>
 							<td>2011-12<br><b>(UPR-2011)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2011-12.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2011-12.pdf')}}" download="2011-12.pdf" data-original-title="Download">Download</a></td>
 						</tr>
 						<tr>
-							<td class="text-center">11</td>
+							<td class="text-center">12</td>
 							<td>Renewal of permissioin for admission of 3<sup>rd</sup> batch of 100 admissions vide letter No. MCI-34(41)/2010-Med/913 dated 14/07/2010</td>
 							<td>2010-11<br><b>(UPR-2010)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2010-11.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2010-11.pdf')}}" download="2010-11.pdf" data-original-title="Download">Download</a></td>
 						</tr>
 						<tr>
-							<td class="text-center">12</td>
+							<td class="text-center">13</td>
 							<td>Renewal of permisison for admisison of 2<sup>nd</sup> Batch of 100 MBBS students vide letter No:U.12012/500/2007-ME.(PII) dated 30/06/2009</td>
 							<td>2009-10<br><b>(UPR-2009)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2009-10.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2009-10.pdf')}}" download="2009-10.pdf" data-original-title="Download">Download</a></td>
 						</tr>
 						<tr>
-							<td class="text-center">13</td>
+							<td class="text-center">14</td>
 							<td>Establishment of new medical college in teh name and style of "Teerthanker Mahaveer Medical College & Research Centre", Moradabad, Uttar Pradesh vide letter No. U.12012/500/2007-ME(P-II) dated 15/07/2008</td>
 							<td>2008-09<br><b>(UPR-2008)</b></td>
 							<td class="text-center"><a data-tooltip="" data-placement="bottom" href="{{asset('/assets/pdf/medical/ug/2008-09.pdf')}}" target="_blank" data-original-title="View">Read More!</a></td>
