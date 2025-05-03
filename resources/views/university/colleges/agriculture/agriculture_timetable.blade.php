e7f5bb67fdd8675380dac553a78f24170507ef01
MathuR-A01 <mathuraayush037@gmail.com>
2025-05-03 15:19:13 +0530

    added the latest timetable in the directory

--- resources/views/university/colleges/agriculture/agriculture_timetable.blade.php
+++ resources/views/university/colleges/agriculture/agriculture_timetable.blade.php
@@ -5,7 +5,7 @@
     <div class="container d-none d-sm-block">
         <div style="text-align:center">
             <h1 class="tmu-text-primary tmu-page-heading mt-md-5"><span>Time </span><span> Table</span></h1>
-            <iframe src="{{asset('/assets/pdf/time_table/Agri_TT_24_25.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
+            <iframe src="{{asset('/assets/pdf/time_table/agriculture/agri_TT_25_26.pdf')}}" frameborder="0" height="800px" width="100%"></iframe>
         </div>
     </div>
     <div class="container d-block d-sm-none mb-3">
@@ -17,9 +17,9 @@
             <div class="iqac-card">
                 <div class="card-pdf">
                     <img src="{{asset('/assets/img/icons/sheet.svg')}}" alt="PDF Icon" class="pdf-icon">
-                    <p class="description">Time Table (Academic Session 2024-25)</p>
+                    <p class="description">Time Table (Academic Session 2025-26)</p>
                     <a class="tmu-btn btn-2 m-0 py-1 px-2 fs-12"
-                        href="{{asset('/assets/pdf/time_table/Agri_TT_24_25.pdf')}}" download>Download PDF</a>
+                        href="{{asset('/assets/pdf/time_table/agriculture/agri_TT_25_26.pdf')}}" download>Download PDF</a>
                 </div>
             </div>
         </div>
