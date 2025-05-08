3effdcf2a86636cf1db1720bf29b5cfbbcfe56ad
MathuR-A01 <mathuraayush037@gmail.com>
2025-05-08 11:45:05 +0530

    replaced the anti ragging committee and added the fresh data in the iqac page

--- resources/views/university/colleges/paramedical/paramedical_anti_ragging.blade.php
+++ resources/views/university/colleges/paramedical/paramedical_anti_ragging.blade.php
@@ -1,129 +1,131 @@
 @extends('layouts.university.colleges.paramedical_with_sidebar')
 @section('content')
-<div class="main-content">
+    <div class="main-content">
 
 
-    <h1 class="tmu-text-primary tmu-page-heading"><span>Anti Ragging </span><span> Committee</span></h1>
-
-    <div class="container">
-        <div class="row mb-3">
-            <div class="table-responsive">
-                <table class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th class="text-center">Sl. No.</th>
-                            <th class="text-center">Name</th>
-                            <th class="text-center">Designation</th>
-                            <th class="text-center">Contact No.</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td class="text-center">1.</td>
-                            <td class="text-center">(Prof.) Dr. Navneet Kumar</td>
-                            <td class="text-center">Chairperson</td>
-                            <td class="text-center">9997378899</td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-center">2.</td>
-                            <td class="text-center">Mr. Baijnath Das</td>
-                            <td class="text-center">Member</td>
-                            <td class="text-center">8505866824</td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-center">3.</td>
-                            <td class="text-center">Mr. Akash Chauhan</td>
-                            <td class="text-center">Member</td>
-                            <td class="text-center">8191980081</td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-center">4.</td>
-                            <td class="text-center">Ms. Anjali Rani</td>
-                            <td class="text-center">Member</td>
-                            <td class="text-center">6398029319</td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-center">5.</td>
-                            <td class="text-center">Mr. Amit Bisht</td>
-                            <td class="text-center">Member</td>
-                            <td class="text-center">7088865263</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        <h1 class="tmu-text-primary tmu-page-heading"><span>Anti Ragging </span><span> Committee</span></h1>
 
         <div class="container">
-            <div class="row justify-content-center mb-5 mt-5">
-                <div class="col-md-8 mb-3 mb-sm-0 ps-3 pe-3" style="background-color: #001055;">
-                    <div class="card border-light my-3">
-                        <div class="card-body text-center">
-                            <h4 class="card-title tmu-text-primary display-6"><span>TOLL FREE ANTI RAGGING </span></h4>
-                            <p class="card-text"><span class="highlight">24X7 HELPLINE NO. 1800-180-5522 </span></p>
+            <div class="row mb-3">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Sl. No.</th>
+                                <th class="text-center">Name</th>
+                                <th class="text-center">Designation</th>
+                                <th class="text-center">Contact No.</th>
+                                <th class="text-center">Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="text-center">1.</td>
+                                <td class="text-center">(Prof.) Dr. Navneet Kumar</td>
+                                <td class="text-center">Chair Person</td>
+                                <td class="text-center">9997378899</td>
+                                <td class="text-center">Principal.paramedical@tmu.ac.in</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">2.</td>
+                                <td class="text-center">Mr. Baijnath Das</td>
+                                <td class="text-center">Member</td>
+                                <td class="text-center">8505866824</td>
+                                <td class="text-center">Baijnath.paramedical@tmu.ac.in</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">3.</td>
+                                <td class="text-center">Mr. Akash Chauhan</td>
+                                <td class="text-center">Member</td>
+                                <td class="text-center">8191980081</td>
+                                <td class="text-center">Akashc.paramedical@tmu.ac.in</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">4.</td>
+                                <td class="text-center">Ms. Anjali Rani</td>
+                                <td class="text-center">Member</td>
+                                <td class="text-center">6398029319</td>
+                                <td class="text-center">Anjalir.paramedical@tmu.ac.in</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">5.</td>
+                                <td class="text-center">Mr. Amit Bisht</td>
+                                <td class="text-center">Member</td>
+                                <td class="text-center">7088865263</td>
+                                <td class="text-center">Amitb.paramedical@tmu.ac.in</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row justify-content-center mb-5 mt-5">
+                    <div class="col-md-8 mb-3 mb-sm-0 ps-3 pe-3" style="background-color: #001055;">
+                        <div class="card border-light my-3">
+                            <div class="card-body text-center">
+                                <h4 class="card-title tmu-text-primary display-6"><span>TOLL FREE ANTI RAGGING </span></h4>
+                                <p class="card-text"><span class="highlight">24X7 HELPLINE NO. 1800-180-5522 </span></p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="row mb-3">
-            <h2 class="tmu-text-primary tmu-page-heading"><span> Proctorial </span><span>Board</span></h2>
-            <div class="table-responsive">
-                <table class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th class="text-center">Sl. No. </th>
-                            <th class="text-center">Name</th>
-                            <th class="text-center">Designation</th>
-                            <th class="text-center">Contact No.</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td class="text-center">1.</td>
-                            <td class="text-center">Mr. Ravi Kumar</td>
-                            <td class="text-center">Proctor</td>
-                            <td class="text-center">9690444424</td>
-                        </tr>
+            <div class="row mb-3">
+                <h2 class="tmu-text-primary tmu-page-heading"><span> Proctorial </span><span>Board</span></h2>
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Sl. No. </th>
+                                <th class="text-center">Name</th>
+                                <th class="text-center">Designation</th>
+                                <th class="text-center">Contact No.</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="text-center">1.</td>
+                                <td class="text-center">Mr. Ravi Kumar</td>
+                                <td class="text-center">Proctor</td>
+                                <td class="text-center">9690444424</td>
+                            </tr>
 
-                        <tr>
-                            <td class="text-center">2.</td>
-                            <td class="text-center">Ms Priyanka Singh</td>
-                            <td class="text-center">Member</td>
-                            <td class="text-center">9536150439</td>
-                        </tr>
+                            <tr>
+                                <td class="text-center">2.</td>
+                                <td class="text-center">Ms Priyanka Singh</td>
+                                <td class="text-center">Member</td>
+                                <td class="text-center">9536150439</td>
+                            </tr>
 
-                        <tr>
-                            <td class="text-center">3.</td>
-                            <td class="text-center">Mr. Rakesh Kumar Yadav</td>
-                            <td class="text-center">Member</td>
-                            <td class="text-center">7387184265</td>
-                        </tr>
+                            <tr>
+                                <td class="text-center">3.</td>
+                                <td class="text-center">Mr. Rakesh Kumar Yadav</td>
+                                <td class="text-center">Member</td>
+                                <td class="text-center">7387184265</td>
+                            </tr>
 
-                        <tr>
-                            <td class="text-center">4.</td>
-                            <td class="text-center">Mr. Shivam Agrawal</td>
-                            <td class="text-center">Member</td>
-                            <td class="text-center">7906961867</td>
-                        </tr>
+                            <tr>
+                                <td class="text-center">4.</td>
+                                <td class="text-center">Mr. Shivam Agrawal</td>
+                                <td class="text-center">Member</td>
+                                <td class="text-center">7906961867</td>
+                            </tr>
 
-                        <tr>
-                            <td class="text-center">5.</td>
-                            <td class="text-center">Mr Arvind Kumar</td>
-                            <td class="text-center">Member</td>
-                            <td class="text-center">9458706894</td>
-                        </tr>
-                    </tbody>
-                </table>
+                            <tr>
+                                <td class="text-center">5.</td>
+                                <td class="text-center">Mr Arvind Kumar</td>
+                                <td class="text-center">Member</td>
+                                <td class="text-center">9458706894</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
-</div>
-<!-- Bootstrap JS and dependencies -->
-
+    </div>
+    <!-- Bootstrap JS and dependencies -->
 @endsection
--- resources/views/university/colleges/paramedical/paramedical_iqac.blade.php
+++ resources/views/university/colleges/paramedical/paramedical_iqac.blade.php
@@ -1,26 +1,58 @@
 @extends('layouts.university.colleges.paramedical_with_sidebar')
 @section('content')
-
-<div class="main-content">
-    <div class="container">
-        <h1 class="tmu-text-primary tmu-page-heading"><span> Internal Quality </span><span> Assurance Cell</span></h1>
+    <div class="main-content">
         <div class="container">
+            <h1 class="tmu-text-primary tmu-page-heading mb-3 mt-3"><span> Internal Quality </span><span> Assurance
+                    Cell</span></h1>
+
             <div class="row mb-3">
+                <h3 class="tmu-text-primary mb-2 mt-2"><span>IQAC </span><span>Committee</span></h3>
                 <div class="table-responsive">
-                    <table class="table table-bordered table-striped">
+                    <table class="table table-bordered table-striped-columns">
                         <thead>
                             <tr>
-                                <th class="text-center fw-bold"> Data Will be updated Soon! </th>
+                                <th class="fw-bold text-center">Sr. No.</th>
+                                <th class="fw-bold">Name</th>
+                                <th class="fw-bold">Designation</th>
+                                <th class="fw-bold">Contact No</th>
+                            </tr>
+                            <tr>
+                                <td class="text-center">1. </td>
+                                <td>Dr. Ruchi Kant </td>
+                                <td>Co-ordinator</td>
+                                <td>+91-7054133711</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">2.</td>
+                                <td>Mr. Rakesh Yadav </td>
+                                <td>Member</td>
+                                <td>+91-7387184265 </td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">3.</td>
+                                <td>Mr. Ravi Kumar </td>
+                                <td>Member</td>
+                                <td>+91-9690444424</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">4.</td>
+                                <td>Mr. Amit Bisht </td>
+                                <td>Member</td>
+                                <td>+91-7088865263</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">5.</td>
+                                <td> Mr. Akash Chauhan </td>
+                                <td>Member</td>
+                                <td>+91-8191980081</td>
                             </tr>
                         </thead>
                     </table>
                 </div>
             </div>
+
         </div>
     </div>
-</div>
-<!-- Bootstrap JS and dependencies -->
-</div>
-
-
+    <!-- Bootstrap JS and dependencies -->
+    </div>
 @endsection
