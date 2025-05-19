916995fe130bfe03ccb9d55bd12c353af459d8a4
MathuR-A01 <mathuraayush037@gmail.com>
2025-05-19 18:01:53 +0530

    craeted new file and inserted the data

--- resources/views/university/iqac/iqac.blade.php
+++ resources/views/university/iqac/iqac.blade.php
@@ -1,88 +1,80 @@
 @extends('layouts.university.departments.iqac_with_sidebar')
 @section('content')
+    <div class="main-content">
+        <div class="container">
+            <!-- Write content for right side here  -->
 
+            <div class="row mx-0 mb-5">
+                <h1 class="tmu-text-primary tmu-page-heading mb-3 pt-md-5"><span>About IQAC</span> <span>(Internal Quality
+                        Assurance Cell)</span></h1>
+                <div id="" class="mb-20 last:mb-0">
+                    <div class="richtext fancylists prose-body mb-1 text-justify">
+                        <p class="mb-2">The <b>Internal Quality Assurance Cell (IQAC)</b> at Teerthanker Mahaveer
+                            University (TMU) is
+                            committed to continuous quality enhancement in all aspects of higher education.
+                            Established in line with the guidelines of the <b>National Assessment and Accreditation
+                                Council (NAAC)</b>, the IQAC functions as a driving force for performance improvement and
+                            institutional excellence.</p>
+                        <p class="mb-2">At TMU, the IQAC plays an important role in promoting quality culture in all
+                            academic and
+                            administrative processes. It acts as a nodal agency for coordinating quality-related activities,
+                            promoting innovative teaching-learning practices, and ensuring the integration of best
+                            practices into institutional functioning. The Cell continually strives to establish quality
+                            benchmarks aligned with national and international ranking and accreditation frameworks,
+                            while also contributing to the formulation of policies that are learner-centric and inclusive in
+                            nature.</p>
+                        <p class="mb-2">The IQAC works to enhance stakeholder engagement, feedback mechanisms, and the
+                            strategic alignment of University activities with its vision and mission and is committed to
+                            nurturing academic excellence, operational efficiency, and a continuous cycle of quality
+                            improvement to achieve the University’s overarching goals of global relevance and societal
+                            impact.</p>
+                    </div>
+                </div>
+                <div class="mb-3 mt-2">
+                    <h2 class="tmu-text-primary text-center mt-4"><span>Message from the </span><span>Chairperson,
+                            IQAC</span></h2>
+                    <div class="quote-box mx-auto">
+                        <div class="profile-image">
+                            <img src="{{ asset('/assets/img/department/iqac/members/2.jpg') }}" alt="Profile Image">
+                        </div>
+                        <div class="quote-text mt-4 mt-md-0">
+                            <h4> <i class="bi bi-quote" style="font-size:50px; color: #737373;"></i>“TMU-IQAC is committed
+                                to continuous monitoring, innovation, and
+                                stakeholder engagement. We drive measurable improvements aligned
+                                with the University&#39;s vision of global relevance. Our efforts focus on
+                                enhancing institutional performance and sustaining excellence. IQAC
+                                remains committed to transformative growth and quality-centric
+                                development.” </h4>
+                            <p class="mt-3">- Prof. (Dr.) V. K. Jain</p>
+                            <p>Chairperson, IQAC</p>
+                        </div>
+                    </div>
+                </div>
 
-<div class="main-content">
-  <div class="container">
-    <!-- Write content for right side here  -->
-
-    <div class="row mx-0 mb-5">
-      <h1 class="tmu-text-primary tmu-page-heading mb-3 pt-md-5"><span>About IQAC</span> <span>(Internal Quality
-          Assurance Cell)</span></h1>
-      <div id="" class="mb-20 last:mb-0">
-        <div class="richtext fancylists prose-body mb-1 text-justify">
-          <p>The Internal Quality Assurance Cell (IQAC) is the cell that is formulated for monitoring the quality
-            parameters in TMU. The Composition of IQAC members as per the recommendations given by National Assessment
-            Accreditation Committee.</p>
+                <div class="mb-5 mt-2">
+                    <h2 class="tmu-text-primary text-center mt-4"><span>Message from the </span><span>Director,
+                            IQAC</span></h2>
+                    <div class="quote-box mx-auto">
+                        <div class="profile-image">
+                            <img src="{{ asset('/assets/img/department/iqac/members/1.jpg') }}" alt="Profile Image">
+                        </div>
+                        <div class="quote-text mt-4 mt-md-0">
+                            <h4> <i class="bi bi-quote" style="font-size:50px; color: #737373;"></i>“Dedicated to promote a
+                                culture of quality across all academic and
+                                administrative function at TMU. Our focus remains on aligning
+                                institutional practices with national and global benchmarks,
+                                enhancing stakeholder engagement, and to integrate best practices
+                                through inclusive, learner-focused policies. We strive for continuous
+                                improvement supporting University&#39;s vision of institutional
+                                excellence and quality education with societal impact.” </h4>
+                            <p class="mt-3">- Prof. (Dr.) Nishith Kumar Mishra</p>
+                            <p>Director, IQAC</p>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
         </div>
-      </div>
-      <div class="heading-block fancy-title border-bottom-0 title-bottom-border mb-2 mt-2">
-        <h4>1. Strategies<span></span></h4>
-      </div>
-      <ol class="m-ol-list ps-5 mb-2 text-justify">
-        <li>To ensure progressive and efficient performance of academic, administrative, and financial tasks in a timely
-          manner.</li>
-        <li>Evolve mechanisms for assessing and ensuring the relevance and quality of academic and research programs.
-        </li>
-        <li>Ensuring equitable access to and affordability of academic programs for various sections of society.</li>
-        <li>Optimization and integration of modern methods of teaching and learning.</li>
-        <li>The credibility of evaluation procedures.</li>
-        <li>Ensuring the adequacy, maintenance, and functioning of the support structure and services.</li>
-        <li>Research sharing and networking with other institutions in India and abroad.</li>
-      </ol>
-
-      <div class="heading-block fancy-title border-bottom-0 title-bottom-border mb-2 mt-2">
-        <h4>2. Functions:<span></span></h4>
-      </div>
-      <ol class="m-ol-list ps-5 mb-2 text-justify">
-        <li>Development and application of quality benchmarks for various academic and administrative activities of
-          various departments and colleges and thus of the University.</li>
-        <li>Facilitating creation of a student-centric environment which is conducive to quality education.</li>
-        <li>Create awareness among the faculty members to adopt the required knowledge and technology for
-          participatory teaching and learning processes.</li>
-        <li>Arrangement for feedback response from students, parents, and other stakeholders on quality-related
-          University processes.</li>
-        <li>Creating awareness on various quality parameters of higher education through seminars, lectures, and
-          workshops.</li>
-        <li>Documentation of the various programs/activities leading to quality improvement.</li>
-        <li>Acting as a nodal agency of the University for coordinating quality-related activities, including adoption
-          and dissemination of best practices.</li>
-        <li>Development and maintenance of University database through ERP for the purpose of maintaining/enhancing
-          institutional quality.</li>
-        <li>Development of Quality Culture in the University.</li>
-        <li>Preparation of the Annual Quality Assurance Report (AQAR) as per guidelines and parameters of NAAC, to be
-          submitted to NAAC.</li>
-      </ol>
-
-      <div class="heading-block fancy-title border-bottom-0 title-bottom-border mb-2 mt-2">
-        <h4>3. IQAC will facilitate / contribute:<span></span></h4>
-      </div>
-      <ol class="m-ol-list ps-5 mb-2 text-justify">
-        <li>Ensure heightened level of clarity and focus in institutional functioning towards quality enhancement.</li>
-        <li>Ensure internalization of the quality culture.</li>
-        <li>Ensure enhancement and coordination among various activities of the institution and institutionalize all
-          good practices.</li>
-        <li>Provide a sound basis for decision-making to improve institutional functioning.</li>
-        <li>Act as a dynamic system for quality changes in HEIs.</li>
-        <li>Build an organized methodology of documentation and internal communication.</li>
-      </ol>
-
-      <div class="heading-block fancy-title border-bottom-0 title-bottom-border mb-2 mt-2">
-        <h4>4. Outcomes of Activities of IQAC at TMU:<span></span></h4>
-      </div>
-      <ol class="m-ol-list ps-5 mb-2 text-justify">
-        <li>NAAC Accreditation</li>
-        <li>Ranking by NIRF (National Institutional Ranking Framework)</li>
-        <li>Annual reports of the University</li>
-        <li>Ranking by Media House(s) conducted through questionnaire and/or surveys</li>
-        <li>Continuous improvement in academic and non-academic activities of the university</li>
-        <li>Analysis of Student/ alumni / faculty/ employer / parent feedbacks</li>
-      </ol>
     </div>
-  </div>
-</div>
-</div>
-
-
-
+    </div>
 @endsection
