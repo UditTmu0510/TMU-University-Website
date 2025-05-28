8338967fbe757486fb3518bad52df5862fbdccf3
[Sarvagyachoudhary] <[sarvagya20065@gmail.com]>
2025-05-28 11:51:01 +0530

    Changes in the js part by adding Defer in it in programme layout

--- resources/views/layouts/programme.blade.php
+++ resources/views/layouts/programme.blade.php
@@ -4,4 +4,8 @@
 @else
 {!!"<h1>No Content section is written for this page</h1>"!!}
 @endif
+
+ <script src="{{ asset('js/modules/bootstrap.js') }}" defer></script>
+    <script src="{{ asset('js/modules/stickfooteronsmall.js') }}" defer></script>
+    <script src="https://widgets.nopaperforms.com/emwgts.js" async></script>
 @include('university.footer')
