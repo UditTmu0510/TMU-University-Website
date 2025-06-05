34cfc71fd81eab60a2c71b0d38901c845c7c4878
MathuR-A01 <mathuraayush037@gmail.com>
2025-06-05 12:14:29 +0530

    replaced the heading and content in the directory

--- resources/views/university/ctld/about_ctld.blade.php
+++ resources/views/university/ctld/about_ctld.blade.php
@@ -3,42 +3,87 @@
     <div class="main-content">
         <div class="container">
             <div class="row mx-0">
-                <h1 class="tmu-text-primary tmu-page-heading mb-2 pt-md-5"><span>About </span><span> Centre for Teaching,
-                        Learning
-                        & Development (CTLD)</span></h1>
+                <h1 class="tmu-text-primary tmu-page-heading mb-2 pt-md-5"><span>Vision, Mission & </span><span> Objectives
+                    </span></h1>
 
-                <h3 class="text-center mb-4 fw-bold tmu-color-red">
+                {{-- <h3 class="text-center mb-4 fw-bold tmu-color-red">
                     Aren’t able to crack the Group Discussion (GD) and Personal Interview (PI) for that dream company?
-                </h3>
-                <h3 class="text-center" style="font-size:1.8vmax"><strong>
+                </h3> --}}
+                {{-- <h3 class="text-center" style="font-size:1.8vmax"><strong>
                         Have we considered what might have gone wrong?</strong>
-                </h3>
+                </h3> --}}
 
-                <p class="mb-4 text-justify">
-                    In today&#39;s competitive job market, academic excellence alone is insufficient. Employers increasingly
-                    prioritise candidates who exhibit strong soft skills, adaptability, and interpersonal abilities.
-                    Recognising this
-                    shift, Teerthanker Mahaveer University (TMU) established the Centre for Teaching, Learning &amp;
-                    Development (CTLD) to bridge the gap between academic knowledge and essential professional
-                    competencies.</p>
-                
-                <img class="mt-4 mb-4" src="{{ asset('/assets/img/banner_overview/ctld/info_1.png') }}" alt="">
-                <p class="mb-4 text-justify">CTLD is committed to the holistic development of students, focusing on enhancing
-                    quantitative aptitude,
-                    logical reasoning, verbal communication, and personality traits. Through structured training programmes,
-                    students are equipped to excel in group discussions, personal interviews, and various professional
-                    scenarios.
-                    These initiatives not only bolster students&#39; confidence but also prepare them to meet the dynamic
-                    demands
-                    of the corporate world.</p>
-               <p class="mb-4 text-justify"> By fostering a culture of continuous learning and personal growth, CTLD ensures that TMU graduates are
-                not only academically proficient but also possess the soft skills necessary to thrive in diverse
-                professional
-                environments.
-                </p>
+                <div class="text-justify mx-auto mb-0 mt">
+                    <h3 class="fw-bold tmu-text-primary mt-2 mb-0"><span></span><span> Vision</span></h3>
+                    <p class="p-1 ">To be a Centre of Excellence for Behavioural, Aptitude &amp; Employability Skills
+                        training to
+                        Students and add value to the Education delivery through continuous Employee development
+                        programmes.</p>
+                </div>
+
+                <div class="text-justify mx-auto mb-0">
+                    <h3 class="fw-bold tmu-text-primary mt-4 mb-0"><span></span><span> Mission</span></h3>
+                    <p class="p-1 ">To foster a Global Mindset in the Students to face real-world challenges and to nurture
+                        &amp;
+                        cultivate better Cognitive, Aptitude, Social, and Behavioural Skills in the Students and
+                        Employees to meet their personal and professional goals.</p>
+                </div>
+
+                <div class="text-justify mx-auto mb-0">
+                    <h3 class="fw-bold tmu-text-primary mt-4 mb-0"><span></span><span> Objectives </span></h3>
+
+                    <p class="p-1">The objective of CTLD is to focus on fostering holistic development across various
+                        domains,
+                        which include the development of behavioural skills in faculty and staff, enhancing soft skills
+                        and grooming the personality of students, building quantitative aptitude among students,
+                        strengthening their reasoning ability, and improving their verbal communication skills.</p>
+                </div>
+
+                <div class="container mb-5">
+                     <h3 class="fw-bold tmu-text-primary mt-4 mb-0"><span></span><span> Focus on </span></h3>
+
+                    <p class="p-1">The Centre for Teaching, Learning &amp; Development (CTLD) primarily serves the following groups:</p>
+                    <div class="row mt-4">
+                        
+                        <div class="col-md-6 col-sm-6">
+                            <div class="media d-flex">
+                                <i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
+                                <div class="media-body">
+                                    <h4 class="mb-1 font-body mt-1">Students</h4>
+                                    <p class="mb-2">Empowering them with essential soft skills, quantitative aptitude,
+                                        communication, and personality development to enhance employability and career
+                                        readiness.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="media d-flex">
+                                <i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
+                                <div class="media-body">
+                                    <h4 class="mb-1 font-body mt-1">Faculty</h4>
+                                    <p class="mb-2">Providing professional development programs to improve teaching
+                                        methodologies, classroom engagement, and continuous learning.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="media d-flex">
+                                <i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
+                                <div class="media-body">
+                                    <h4 class="mb-1 font-body mt-1">Staff</h4>
+                                    <p class="mb-2">Offering workshops aimed at improving workplace communication,
+                                        teamwork,
+                                        and administrative efficiency.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <p class="p-1"> CTLD’s holistic approach ensures that all members of the university community are
+                            equipped
+                            to meet academic and professional challenges effectively.</p>
+                    </div>
+                </div>
 
             </div>
-
         </div>
     </div>
     </div>
